c4defc018ea7f7c7b8299e054355847bbc49f1f6
mantuok <mantuok@gmail.com>
2024-09-25 19:27:07 -0700

    updated page not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,18 @@
 import { Button, Heading, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
-const PageNotFound = () => {
+const NotFound = () => {
   return (
     <>
       <Heading>404 - Page Not Found</Heading>
       <Text>Sorry, but the page you are looking for does not exist.</Text>
       <Link href="/">
-        <Button colorScheme="teal">Go Back to Home</Button>
+        <Button size="sm" colorScheme="teal">
+          Go Back to Home
+        </Button>
       </Link>
     </>
   );
 };
 
-export default PageNotFound;
+export default NotFound;
