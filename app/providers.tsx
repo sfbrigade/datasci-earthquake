7a1a537dfe5dbccc70682b70a9a44a752af0bbda
mantuok <mantuok@gmail.com>
2024-09-27 10:57:55 -0700

    Clean up and configure web app (#47)
    * cleared page.tsx, added blank lines
    
    * cleared layout.tsx
    
    * cleared global.css
    
    * installed Prettier, configured Prettier and ESLint
    
    * Updated prettierignore list

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider } from '@chakra-ui/react';
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "../styles/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
