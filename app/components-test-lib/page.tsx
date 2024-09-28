c4e644a549b7fc5ff7ca38b6125cc08c8cf8b6fb
mantuok <mantuok@gmail.com>
2024-09-27 19:31:42 -0700

    created components test page

---
+++ app/components-test-lib/page.tsx
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+
+const ComponentsTestLib = () => {
+  if (process.env.NODE_ENV !== "development") {
+    return <div>Page not available in production.</div>;
+  }
+
+  return <Heading>This is Components Test Library</Heading>;
+};
+
+export default ComponentsTestLib;
