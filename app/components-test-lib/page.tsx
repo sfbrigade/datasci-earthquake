74cbf4896efb0128d3a81b02e73bf3f3ebb7292e
mantuok <mantuok@gmail.com>
2024-09-30 18:43:00 -0700

    Mock button updated and added to test lib

--- app/components-test-lib/page.tsx
+++ app/components-test-lib/page.tsx
@@ -1,13 +1,29 @@
-import { Heading } from "@chakra-ui/react";
+"use client";
+
+import { Heading, useToast } from "@chakra-ui/react";
+import MockButton from "../components/mock-button";
 
 const ComponentsTestLib = () => {
+  const toast = useToast();
+
   if (process.env.NODE_ENV !== "development") {
     return <div>Page not available in production.</div>;
   }
 
+  const showToast = () => {
+    toast({
+      title: "Action Successful.",
+      description: "You've clicked the button!",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+    });
+  };
+
   return (
     <>
-      <Heading>This is Components Test Library</Heading>;
+      <Heading>This is Components Test Library</Heading>
+      <MockButton label="Click me!" onClick={showToast} />
     </>
   );
 };
--- app/components/mock-button.tsx
+++ app/components/mock-button.tsx
@@ -1,23 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
 interface MockButtonProps {
   label?: string;
   onClick?: () => void;
 }
 
 const MockButton = ({ label = "Click Me", onClick }: MockButtonProps) => {
-  return (
-    <button
-      onClick={onClick}
-      style={{
-        padding: "10px 20px",
-        backgroundColor: "lightgray",
-        border: "1px solid #ccc",
-        borderRadius: "4px",
-        cursor: "pointer",
-      }}
-    >
-      {label}
-    </button>
-  );
+  return <Button onClick={onClick}>{label}</Button>;
 };
 
 export default MockButton;
