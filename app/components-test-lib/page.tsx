49e87fe1157f7931d531e2ccbae4e2354de5e5a7
mantuok <mantuok@gmail.com>
2024-10-02 15:51:41 -0700

    components lib added and mock button updated

--- app/components-test-lib/page.tsx
+++ app/components-test-lib/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Heading, useToast } from "@chakra-ui/react";
+import { Box, Heading, VStack, useToast, Text, HStack } from "@chakra-ui/react";
 import MockButton from "../components/mock-button";
 
 const ComponentsTestLib = () => {
@@ -10,10 +10,10 @@ const ComponentsTestLib = () => {
     return <div>Page not available in production.</div>;
   }
 
-  const showToast = () => {
+  const showToast = (message: string) => {
     toast({
       title: "Action Successful.",
-      description: "You've clicked the button!",
+      description: message,
       status: "success",
       duration: 5000,
       isClosable: true,
@@ -22,8 +22,44 @@ const ComponentsTestLib = () => {
 
   return (
     <>
-      <Heading>This is Components Test Library</Heading>
-      <MockButton label="Click me!" onClick={showToast} />
+      <Box as="section" p="8">
+        <Heading as="h1" size="xl" mb={6}>
+          Components Test Library
+        </Heading>
+        <Heading as="h2" size="md" mb={4}>
+          Button Variants
+        </Heading>
+        <Text mb={4}>
+          This section demonstrates different states of the Button component
+        </Text>
+        <HStack spacing={4} align="center">
+          <VStack>
+            <Heading as="h3" size="sm">
+              Default
+            </Heading>
+            <MockButton
+              label="Default"
+              onClick={() => showToast("Default Button Clicked")}
+            />
+          </VStack>
+          <VStack>
+            <Heading as="h3" size="sm">
+              Disabled
+            </Heading>
+            <MockButton label="Disabled" onClick={() => {}} disabled={true} />
+          </VStack>
+          <VStack>
+            <Heading as="h3" size="sm">
+              Loading
+            </Heading>
+            <MockButton
+              label="Loading..."
+              onClick={() => showToast("Loading Button Clicked")}
+              isLoading={true}
+            />
+          </VStack>
+        </HStack>
+      </Box>
     </>
   );
 };
--- app/components/mock-button.tsx
+++ app/components/mock-button.tsx
@@ -1,12 +1,23 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Spinner } from "@chakra-ui/react";
 
 interface MockButtonProps {
   label?: string;
   onClick?: () => void;
+  disabled?: boolean;
+  isLoading?: boolean;
 }
 
-const MockButton = ({ label = "Click Me", onClick }: MockButtonProps) => {
-  return <Button onClick={onClick}>{label}</Button>;
+const MockButton = ({
+  label = "Click Me",
+  onClick,
+  isLoading = false,
+  disabled = false,
+}: MockButtonProps) => {
+  return (
+    <Button onClick={onClick} isDisabled={disabled} minWidth="100px">
+      {isLoading ? <Spinner size="sm" /> : label}
+    </Button>
+  );
 };
 
 export default MockButton;
