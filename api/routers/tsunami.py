decab218dbf70e94b5e56a4c8aed475a090eb71d
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2024-10-03 18:40:37 -0400

    Copied combined changes.

---
+++ api/routers/tsunami.py
@@ -0,0 +1,19 @@
+"""Router to get tsunami risk and CRUD tsunami polygons."""
+from fastapi import APIRouter
+from ..tags import Tags
+
+
+router = APIRouter(
+    prefix="/api/tsunami",
+    tags=[Tags.TSUNAMI],
+)
+
+
+@router.get("/risk/{address}")
+async def get_risk(address: str) -> bool:
+    """
+    Return whether this address is in the current tsunami risk
+    polygon.
+    """
+    # TODO: Change return type to boolean to avoid validation error
+    pass
