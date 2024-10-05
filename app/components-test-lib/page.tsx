cd5a4deb9062940c6b40983727d37f21e0d29f0e
mantuok <mantuok@gmail.com>
2024-10-05 16:38:07 -0700

    updated according to comments

--- app/components-test-lib/page.tsx
+++ app/components-test-lib/page.tsx
@@ -6,10 +6,6 @@ import MockButton from "../components/mock-button";
 const ComponentsTestLib = () => {
   const toast = useToast();
 
-  if (process.env.NODE_ENV !== "development") {
-    return <div>Page not available in production.</div>;
-  }
-
   const showToast = (message: string) => {
     toast({
       title: "Action Successful.",
@@ -21,46 +17,44 @@ const ComponentsTestLib = () => {
   };
 
   return (
-    <>
-      <Box as="section" p="8">
-        <Heading as="h1" size="xl" mb={6}>
-          Components Test Library
-        </Heading>
-        <Heading as="h2" size="md" mb={4}>
-          Button Variants
-        </Heading>
-        <Text mb={4}>
-          This section demonstrates different states of the Button component
-        </Text>
-        <HStack spacing={4} align="center">
-          <VStack>
-            <Heading as="h3" size="sm">
-              Default
-            </Heading>
-            <MockButton
-              label="Default"
-              onClick={() => showToast("Default Button Clicked")}
-            />
-          </VStack>
-          <VStack>
-            <Heading as="h3" size="sm">
-              Disabled
-            </Heading>
-            <MockButton label="Disabled" onClick={() => {}} disabled={true} />
-          </VStack>
-          <VStack>
-            <Heading as="h3" size="sm">
-              Loading
-            </Heading>
-            <MockButton
-              label="Loading..."
-              onClick={() => showToast("Loading Button Clicked")}
-              isLoading={true}
-            />
-          </VStack>
-        </HStack>
-      </Box>
-    </>
+    <Box as="section" p="8">
+      <Heading as="h1" size="xl" mb={6}>
+        Components Test Library
+      </Heading>
+      <Heading as="h2" size="md" mb={4}>
+        Button Variants
+      </Heading>
+      <Text mb={4}>
+        This section demonstrates different states of the Button component
+      </Text>
+      <HStack spacing={4} align="center">
+        <VStack>
+          <Heading as="h3" size="sm">
+            Default
+          </Heading>
+          <MockButton
+            label="Default"
+            onClick={() => showToast("Default Button Clicked")}
+          />
+        </VStack>
+        <VStack>
+          <Heading as="h3" size="sm">
+            Disabled
+          </Heading>
+          <MockButton label="Disabled" onClick={() => {}} disabled={true} />
+        </VStack>
+        <VStack>
+          <Heading as="h3" size="sm">
+            Loading
+          </Heading>
+          <MockButton
+            label="Loading..."
+            onClick={() => showToast("Loading Button Clicked")}
+            isLoading={true}
+          />
+        </VStack>
+      </HStack>
+    </Box>
   );
 };
 
