8468059691813efbd74698ebac7f95696a8c3093
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2024-10-09 22:30:50 -0400

    Backend (#55)
    * Copied combined changes.
    
    * Added pytest.
    
    * Split models by concern.
    
    * Moved each model into own python file.
    
    * Deleted test_index.py
    
    * Pruned TODO

---
+++ api/models/tsunami.py
@@ -0,0 +1,33 @@
+"""Tsunami Risk Zone data"""
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from geoalchemy2 import Geometry
+from datetime import datetime, DateTime
+
+
+MAPPED_COLUMN_STRING_LENGTH = 200
+
+
+class TsunamiZones(DeclarativeBase):
+    """
+    All data of the Tsunami Hazard table from conservation.ca.gov.
+    """
+    __tablename__ = "tsunami_zones"
+    
+    identifier: Mapped[int] = mapped_column(Integer, primary_key=True)
+    evacuate: Mapped[str] = mapped_column(String(MAPPED_COLUMN_STRING_LENGTH))
+    county: Mapped[str] = mapped_column(String(MAPPED_COLUMN_STRING_LENGTH))
+    gis_link: Mapped[str] = mapped_column(String(MAPPED_COLUMN_STRING_LENGTH))
+    kmz_link: Mapped[str] = mapped_column(String(MAPPED_COLUMN_STRING_LENGTH))
+    map_link: Mapped[str] = mapped_column(String(MAPPED_COLUMN_STRING_LENGTH))
+    label: Mapped[str] = mapped_column(String(MAPPED_COLUMN_STRING_LENGTH))
+    shape_length: Mapped[float] = mapped_column(Float)
+    shape_area: Mapped[float] = mapped_column(Float)
+    # This data is ingested as PolygonZ but should be stored as MultiPolygon
+    geometry: Mapped[Geometry] = mapped_column(Geometry('MULTIPOLYGON', srid=4326))
+    update_timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"<TsunamiZones(id={self.identifier})>"
---
+++ api/tests/test_soft_story.py
@@ -0,0 +1,41 @@
+"""
+Test the API of soft_story.py.
+"""
+import pytest
+from fastapi.testclient import TestClient
+# Will the .. be stable?
+from ..index import app
+from ..schemas import Polygon
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_delete_soft_story(client):
+    response = client.delete("/api/soft-story/address")
+    assert response.status_code == 200
+    # Temporary guaranteed failure until test is written
+    assert False
+
+
+def test_put_soft_story(client):
+    response = client.put("/api/soft-story/address?soft-story=true")
+    assert response.status_code == 200
+    # Temporary guaranteed failure until test is written
+    assert False
+
+
+def test_post_soft_story(client):
+    response = client.put("/api/soft-story/address?soft-story=true")
+    assert response.status_code == 200
+    # Temporary guaranteed failure until test is written
+    assert False
+
+
+def test_get_soft_story(client):
+    response = client.get("/api/soft-story/address")
+    assert response.status_code == 200
+    # Temporary guaranteed failure until test is written
+    assert False
