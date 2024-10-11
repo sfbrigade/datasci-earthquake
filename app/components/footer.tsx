9bf2fd94a820336c57f1d5c2703af29c2c959b4c
mantuok <mantuok@gmail.com>
2024-10-11 16:56:24 -0700

    refactoring

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,8 +3,8 @@ import { Box } from "@chakra-ui/react";
 const Footer = () => {
   return (
     <Box
-      width={{ base: "base", lg: "lg" }}
-      padding={{
+      w={{ base: "base", lg: "lg" }}
+      p={{
         base: "19px 23px 8px 23px",
         md: "26px 27px 14px 26px",
         lg: "29px 127px 13px 127px",
--- app/page.tsx
+++ app/page.tsx
@@ -5,37 +5,40 @@ const Home = () => {
     <Flex direction="column">
       <Box bgColor="primary.blue">
         <Box
-          width={{ base: "base", lg: "lg" }}
-          padding={{
+          h={{ base: "166px", md: "213px", lg: "261px" }}
+          w={{ base: "base", lg: "lg" }}
+          p={{
             base: "45px 23px 50px 23px",
             md: "52px 27px 56px 26px",
             lg: "53px 127px 46px 127px",
           }}
-          margin="auto"
+          m="auto"
         >
           Promo and search box
         </Box>
       </Box>
       <Box
-        width={{ base: "base", lg: "lg" }}
-        padding={{
+        w={{ base: "base", lg: "lg" }}
+        h={{ base: "166px", md: "213px", lg: "261px" }}
+        p={{
           base: "23px 23px 27px 23px",
           md: "37px 27px 28px 26px",
           lg: "50px 127px 40px 127px",
         }}
-        margin="auto"
+        m="auto"
       >
         Cards and map
       </Box>
       <Box bgColor="primary.blue">
         <Box
-          width={{ base: "base", lg: "lg" }}
-          padding={{
+          w={{ base: "base", lg: "lg" }}
+          h={{ base: "166px", md: "213px", lg: "261px" }}
+          p={{
             base: "26px 23px 28px 23px",
             md: "37px 23px 28px 24px",
             lg: "24px 127px 22px 127px",
           }}
-          margin="auto"
+          m="auto"
         >
           Cards
         </Box>
