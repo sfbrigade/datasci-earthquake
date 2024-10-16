6b42bcec45b10fbf1d7cac42561af3d3970e7fdb
mantuok <mantuok@gmail.com>
2024-10-16 08:58:48 -0700

    Add design tokens (#62)
    * added and applied colors
    
    * font added
    
    * text styles added
    
    * text styles added
    
    * sizes and paddings updated
    
    * font colors added to textStyles
    
    * updated logo font weight

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,10 +1,55 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const customTheme = extendTheme({
-  colors: {
-    primary: {
-      blue: "#2C5282",
+  fonts: {
+    body: "Inter, sans-serif",
+    heading: "Inter, sans-serif",
+  },
+  textStyles: {
+    logo: {
+      fontSize: ["xl", "xl", "2xl", "2xl", "2xl", "2xl"],
+      fontWeight: "normal",
+      color: "blue",
     },
+    headerBig: {
+      fontSize: ["4xl", "4xl", "5xl", "5xl", "6xl", "6xl"],
+      fontWeight: "normal",
+      color: "white",
+    },
+    headerMedium: {
+      fontSize: ["2xl", "2xl", "3xl", "3xl", "3xl", "3xl"],
+      fontWeight: "bold",
+      color: "blue",
+    },
+    headerSmall: {
+      fontSize: ["lg", "lg", "lg", "lg", "xl", "xl"],
+      fontWeight: "bold",
+      color: "white",
+    },
+    textMedium: {
+      fontSize: "md",
+      fontWeight: "normal",
+      color: "grey.900",
+    },
+    textSmall: {
+      fontSize: "xs",
+      fontWeight: "normal",
+      color: "grey.900",
+    },
+  },
+  colors: {
+    grey: {
+      200: "#E2E8F0",
+      400: "#A0AEC0",
+      900: "#171923",
+    },
+    white: "#FFF",
+    blue: "#2C5282",
+    yellow: "#ECC94B",
+    red: "#E53E3E",
+    green: "#25855A",
+    orange: "#F6AD55",
+    pink: "#ED64A6",
   },
   space: {
     12: "128px",
@@ -15,13 +60,15 @@ const customTheme = extendTheme({
     base: "100%",
     sm: "375px",
     md: "744px",
-    lg: "1280px",
+    xl: "1280px",
   },
   breakpoints: {
     base: "0px",
     sm: "375px",
     md: "744px",
-    lg: "1280px",
+    lg: "992px",
+    xl: "1280px",
+    "2xl": "1536px",
   },
 });
 
