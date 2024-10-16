af5db6a7db264b689ed20b77ab1b2cea511d6619
mantuok <mantuok@gmail.com>
2024-10-16 08:58:48 -0700

    Add design tokens (#62)
    * added and applied colors
    
    * font added
    
    * text styles added
    
    * text styles added
    
    * sizes and paddings updated
    
    * font colors added to textStyles
    
    * updated logo font weight

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,16 +1,18 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 const Header = () => {
   return (
     <Box
-      w={{ base: "base", lg: "lg" }}
+      w={{ base: "base", xl: "xl" }}
       p={{
         base: "19px 23px 8px 23px",
         md: "26px 27px 14px 26px",
-        lg: "29px 127px 13px 127px",
+        xl: "29px 127px 13px 127px",
       }}
     >
-      <Box border="1px solid grey">Header</Box>
+      <Box border="1px solid" borderColor="grey.400">
+        <Text textStyle="logo">Logo</Text>
+      </Box>
     </Box>
   );
 };
