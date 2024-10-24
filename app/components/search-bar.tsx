00347285897e5c98412e7345a51888536698b481
mantuok <mantuok@gmail.com>
2024-10-23 19:03:04 -0700

    search bar style in process

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -1,7 +1,26 @@
-import { Input } from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { IoSearchSharp } from "react-icons/io5";
 
 const SearchBar = () => {
-  return <Input placeholder="Search San Francisco address" />;
+  return (
+    <InputGroup
+      maxW={{ base: "303px", md: "371px", xl: "417px" }}
+      size={{ base: "md", md: "lg", xl: "lg" }}
+    >
+      <InputLeftElement>
+        <IoSearchSharp color="#2C5282" fontSize="1.1em" />
+      </InputLeftElement>
+      <Input
+        placeholder="Search San Francisco address"
+        p="0 10px 0 48px"
+        borderRadius="50"
+        bgColor="white"
+        focusBorderColor="grey.400"
+        // maxW={{ base: "303px", md: "371px", xl: "417px" }}
+        // size={{ base: "md", md: "lg", xl: "lg" }}
+      />
+    </InputGroup>
+  );
 };
 
 export default SearchBar;
