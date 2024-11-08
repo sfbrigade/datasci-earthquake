dec6203f8e1e68557fef834ee33f83a198fb75ba
Svetlana Eliseeva <mantuok@gmail.com>
2024-11-07 20:14:35 -0800

    responsive styling updateed

--- app/components/search-bar.jsx
+++ app/components/search-bar.jsx
@@ -8,16 +8,25 @@ import { AddressAutofill } from '@mapbox/search-js-react';
 
 
 const SearchBar = () => {
-const [address, setAddress] = useState("");
+  const [address, setAddress] = useState("");
+  const [fullAddress, setFullAddress] = useState(null);
 
-const handleClearClick = () => {
-  setAddress("");
-};
+  const handleClearClick = () => {
+    console.log(address);
+    console.log(fullAddress);
+    setAddress("");
+  };
+
+  const handleRetrieve = (event) => {
+    const addressData = event.features[0];
+    setFullAddress(addressData);
+  };
 
   return (
     <form>
       <AddressAutofill
         accessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        onRetrieve={handleRetrieve}
       >
         <InputGroup
           maxW={{ base: "303px", sm: "303px", md: "371px", lg: "417px" }}
@@ -33,7 +42,8 @@ const handleClearClick = () => {
           </InputLeftElement>
           <Input
             placeholder="Search San Francisco address"
-            p="0 10px 0 48px"
+            fontSize={{ base: "md", sm: "md", md: "md", lg: "lg" }}
+            p={{ base: "0 10px 0 35px", sm: "0 10px 0 35px", md: "0 10px 0 48px", lg: "0 10px 0 48px" }}
             borderRadius="50"
             bgColor="white"
             focusBorderColor="yellow"
