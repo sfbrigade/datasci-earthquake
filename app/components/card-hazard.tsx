298611968c0b17d2021ffa1b0cd17ba31aab825b
Svetlana Eliseeva <mantuok@gmail.com>
2024-11-19 14:00:47 -0800

    added tests for hazards card

--- app/components/card-hazard.tsx
+++ app/components/card-hazard.tsx
@@ -15,11 +15,12 @@ const CardHazard = () => {
   return (
     <Card maxW={332}>
       <CardHeader
-      p={{
-        base: "10px 23px 0px 23px",
-        md: "17px 16px 0px 16px",
-        xl: "22px 22px 0px 22px",
-      }}>
+        p={{
+          base: "10px 23px 0px 23px",
+          md: "17px 16px 0px 16px",
+          xl: "22px 22px 0px 22px",
+        }}
+      >
         <HStack justifyContent="space-between">
           <Text textStyle="textBig">{hazard.title}</Text>
           <Pill />
@@ -30,15 +31,17 @@ const CardHazard = () => {
           base: "10px 23px 0px 23px",
           md: "17px 16px 0px 16px",
           xl: "14px 22px 0px 22px",
-        }}>
+        }}
+      >
         <Text textStyle="textMedium">{hazard.description}</Text>
       </CardBody>
-      <CardFooter 
+      <CardFooter
         p={{
           base: "10px 23px 10px 23px",
           md: "17px 16px 17px 16px",
           xl: "14px 22px 22px 22px",
-        }}>
+        }}
+      >
         <HStack>
           <svg width="19" height="18" viewBox="0 0 19 18" fill="none">
             <circle
@@ -47,6 +50,7 @@ const CardHazard = () => {
               r="8.5"
               fill={hazard.color}
               stroke="white"
+              role="img"
             />
           </svg>
           <Text textStyle="textSmall">{"Updated " + hazard.update}</Text>
