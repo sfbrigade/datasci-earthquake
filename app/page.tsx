8560efd93e990933a3b859106079267243dfea02
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2024-11-20 22:48:59 -0800

    change map to be edge-to-edge

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,7 @@ const Home = () => {
       </Box>
       <Box
         w={{ base: "base", xl: "xl" }}
+        // w="base"
         p={{
           base: "23px 24px 16px 24px",
           md: "37px 27px 16px 26px",
@@ -37,22 +38,11 @@ const Home = () => {
       >
         <Report />
       </Box>
-      <Box
-        w={{ base: "base", md: "741px", xl: "xl" }}
-        h={{ base: "323px", md: "411px", xl: "462px" }}
-        p={{
-          base: "0px 24px 27px 24px",
-          md: "0px 27px 28px 26px",
-          xl: "0px 128px 40px 127px",
-        }}
-        m="auto"
-      >
+      <Box w="base" h={{ base: "323px", md: "411px", xl: "462px" }} m="auto">
         <Box
           h="100%"
           border="1px solid"
           borderColor="grey.400"
-          borderRadius="8px"
-          boxShadow="0px 2px 4px -1px #0000000F, 0px 4px 6px -1px #0000001A"
           overflow="hidden"
         >
           <Map />
