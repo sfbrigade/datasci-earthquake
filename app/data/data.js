c23a1bae59f2ee4c11961ef6d0d141c08048bd90
Svetlana Eliseeva <mantuok@gmail.com>
2024-12-03 16:05:02 -0800

    added information data

--- app/data/data.js
+++ app/data/data.js
@@ -27,3 +27,57 @@ export const Hazards = [
     color: "#ED64A6",
   },
 ];
+
+export const Info = [
+  {
+    id: 1,
+    name: "preparedness",
+    title: "Preparedness",
+    list: [
+      {
+        id: 1,
+        name: "Ready.gov",
+        ulr: "https://www.ready.gov/",
+      },
+      {
+        id: 2,
+        name: "Earthquake Authority",
+        ulr: "https://www.earthquakeauthority.com/",
+      },
+      {
+        id: 3,
+        name: "MyShake app",
+        ulr: "https://myshake.berkeley.edu/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "services",
+    title: "Building retrofit services",
+    list: [
+      {
+        id: 1,
+        name: "Find a contractor",
+        ulr: "https://www.californiaresidentialmitigationprogram.com/resources/find-a-contractor/",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "grants",
+    title: "Retrofit grants",
+    list: [
+      {
+        id: 1,
+        name: "Earthquake Brace + Bolt program",
+        ulr: "https://www.californiaresidentialmitigationprogram.com/our-seismic-retrofit-programs/the-retrofits/ebb-retrofit",
+      },
+      {
+        id: 2,
+        name: "EarthquakeSoftStory.com",
+        ulr: "https://www.californiaresidentialmitigationprogram.com/our-seismic-retrofit-programs/the-retrofits/ess-retrofit",
+      },
+    ],
+  },
+];
