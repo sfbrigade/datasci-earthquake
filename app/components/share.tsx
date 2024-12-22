d84157ca4e99c079f130a67689d76fb2e68913ea
amnak613 <amnak1284@gmail.com>
2024-12-22 13:13:00 -0800

    created links to sites

--- app/components/share.tsx
+++ app/components/share.tsx
@@ -1,3 +1,5 @@
+
+'use client'
 import {
   Text,
   Button,
@@ -11,8 +13,25 @@ import FacebookIcon from "../img/icon-facebook.svg";
 import EmailIcon from "../img/icon-email.svg";
 import XIcon from "../img/icon-x.svg";
 import LinkIcon from "../img/icon-link.svg";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { AddressData } from "./__mocks__/address-data";
+import Link from "next/link";
 
 const Share = () => {
+  const handleClick = () => {
+    console.log(currentUrl);
+  };
+
+  const [currentUrl, setCurrentUrl] = useState('');
+  
+  useEffect(() => {
+  
+ 
+    setCurrentUrl(window.location.href)
+    console.log(currentUrl);
+  }, []);
+
   return (
     <Menu>
       <MenuButton
@@ -26,19 +45,30 @@ const Share = () => {
       <MenuList p={"6px 16px 6px 16px"}>
         <MenuItem gap="10px">
           <EmailIcon />
-          <Text>Email</Text>
+          <Link href={'mailto:'}>
+          <Text>
+        
+            Email</Text>
+            </Link>
+          
         </MenuItem>
         <MenuItem gap="10px">
+          <Link  href={''}/*href={"https://www.facebook.com/sharer/sharer.php?"}*/>
           <FacebookIcon />
           <Text>Facebook</Text>
+          </Link>
         </MenuItem>
-        <MenuItem gap="10px">
+        <MenuItem gap="10px">""
+          <Link href={"https://twitter.com/intent/tweet?"}>
           <XIcon />
           <Text>X</Text>
+          </Link>
         </MenuItem>
         <MenuItem gap="10px">
+         
           <LinkIcon />
           <Text>Copy Link</Text>
+    
         </MenuItem>
       </MenuList>
     </Menu>
