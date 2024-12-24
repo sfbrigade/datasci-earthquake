879db9fc950c56ed99d6cb99f1ab99a374c8291e
Eli Lucherini <elucherini@users.noreply.github.com>
2024-12-24 09:24:47 -0800

    DB creation hotfix (#134)

--- backend/database/init_db.py
+++ backend/database/init_db.py
@@ -34,6 +34,12 @@ Caution:
 from backend.api.models.base import Base
 from sqlalchemy import inspect
 from backend.database.session import engine
+from backend.api.models.addresses import Address
+from backend.api.models.tsunami import TsunamiZone
+from backend.api.models.landslide_zones import LandslideZone
+from backend.api.models.seismic_hazard_zones import SeismicHazardZone
+from backend.api.models.liquefaction_zones import LiquefactionZone
+from backend.api.models.soft_story_properties import SoftStoryProperty
 
 
 def init_db():
