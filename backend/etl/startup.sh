d3ed9c8ac0c351be06a4c6b582274c3bd49bfe0a
Anna <annagennadinik@gmail.com>
2025-01-01 19:22:12 -0800

    updated startup.sh

--- backend/etl/startup.sh
+++ backend/etl/startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 python backend/database/init_db.py
 python backend/etl/address_data_handler.py
 python backend/etl/landslide_data_handler.py
@@ -6,3 +8,6 @@ python backend/etl/liquefaction_data_handler.py
 python backend/etl/seismic_data_handler.py
 python backend/etl/soft_story_properties_data_handler.py
 python backend/etl/tsunami_data_handler.py
+
+# Start FastAPI
+uvicorn api.main:app --host 0.0.0.0 --port 8000
