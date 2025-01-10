8486ed38569d266cf0db1364a672a4c6fcf47212
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-01-10 12:52:51 -0800

    add comment for dynamic address autofill

--- app/components/address-autofill.tsx
+++ app/components/address-autofill.tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
-
+// NOTE: we are forcing this third party lib to be loaded with `ssr = false`
+// because it uses the `document` object, which is client-side only
 const DynamicAddressAutofill = dynamic(
   () => import("@mapbox/search-js-react").then((mod) => mod.AddressAutofill),
   { ssr: false }
