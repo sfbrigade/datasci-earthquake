1576352e87ae1f7b8be9a56b64af8b0812d177ec
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-01-11 20:08:15 -0500

    Updated test database.
    Co-authored-by: Eli Lucherini <elucherini@users.noreply.github.com>

--- backend/api/tests/test_soft_story.py
+++ backend/api/tests/test_soft_story.py
@@ -2,7 +2,7 @@ from backend.api.tests.test_session_config import test_engine, test_session, cli
 
 
 def test_get_soft_stories(client):
-    response = client.get(f"/soft-stories/")
+    response = client.get("/soft-stories/")
     response_dict = response.json()
     assert response.status_code == 200
     assert len(response_dict["features"]) == 6
