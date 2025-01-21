6ea1d125e8f6c0487d1fc12f948715a8d276d636
nladio <connect@nlad.io>
2025-01-20 18:55:20 -0800

    1. Add vartype of ModelType. 2. Use Type[ModelType] for `table` props in the `etl/data_handler` and `soft_story_properties_data_handler` constructors.

--- backend/api/models/base.py
+++ backend/api/models/base.py
@@ -1,5 +1,10 @@
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.declarative import DeclarativeMeta
+from typing import TypeVar
 
 
 class Base(DeclarativeBase):
     pass
+
+
+ModelType = TypeVar("ModelType", bound=DeclarativeBase)
