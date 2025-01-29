f038528bf2daa78753f15d53d7c746ef5720b2f7
Anna <annagennadinik@gmail.com>
2025-01-29 15:33:38 -0800

    added entrypoint for serverless funcs

---
+++ api/index.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.api.routers import liquefaction_api, tsunami_api, soft_story_api
+
+### Create FastAPI instance with custom docs and openapi url
+app = FastAPI(
+    docs_url="/api/docs", openapi_url="/api/openapi.json", redirect_slashes=False
+)
+
+"""app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust to your needs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)"""
+
+app.include_router(liquefaction_api.router)
+app.include_router(tsunami_api.router)
+app.include_router(soft_story_api.router)
+
+
+@app.get("/api/helloFastApi")
+def hello_fast_api():
+    return {"message": "Hello from FastAPI"}
