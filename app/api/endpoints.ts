c5f1054637a114160e361c51be79098d757e23e9
Anna <annagennadinik@gmail.com>
2025-02-03 16:06:02 -0800

    removed trailing slashes

--- app/api/endpoints.ts
+++ app/api/endpoints.ts
@@ -1,7 +1,7 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const ENDPOINTS = {
-  softStories: `${BASE_URL}/soft-stories/`,
-  tsunami: `${BASE_URL}/tsunami-zones/`,
-  liquefaction: `${BASE_URL}/liquefaction-zones/`,
+  softStories: `${BASE_URL}/soft-stories`,
+  tsunami: `${BASE_URL}/tsunami-zones`,
+  liquefaction: `${BASE_URL}/liquefaction-zones`,
 };
