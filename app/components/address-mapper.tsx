275df0043c419a91640b75109f9a51d9d04cd3ed
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-02-05 18:50:14 -0800

    move search and map into client component

---
+++ app/components/address-mapper.tsx
@@ -0,0 +1,68 @@
+import { Box, Flex } from "@chakra-ui/react";
+import SearchBar from "./search-bar";
+import Heading, { HeadingProps } from "./heading";
+import Map from "./map";
+import Report from "./report";
+import Information from "./information";
+
+const addressLookupCoordinates = {
+  geometry: {
+    type: "Point",
+    coordinates: [-122.408020683, 37.801698301],
+  },
+};
+const coords = addressLookupCoordinates.geometry.coordinates ?? [];
+
+interface AddressMapperProps {
+  coords: number[];
+  headingData: HeadingProps;
+  softStoryData: {};
+  tsunamiData: {};
+  liquefactionData: {};
+}
+
+const AddressMapper: React.FC<AddressMapperProps> = ({
+  coords,
+  headingData,
+  softStoryData,
+  tsunamiData,
+  liquefactionData,
+}) => {
+  return (
+    <Flex direction="column">
+      <Box bgColor="blue">
+        <Box
+          w={{ base: "base", xl: "xl" }}
+          p={{
+            base: "45px 23px 50px 23px",
+            md: "52px 260px 56px 26px",
+            xl: "53px 470px 46px 127px",
+          }}
+          m="auto"
+        >
+          <Heading headingData={headingData} />
+          <SearchBar />
+        </Box>
+      </Box>
+      <Box w="base" h={{ base: "1400px", md: "1000px" }} m="auto">
+        <Box
+          h="100%"
+          border="1px solid"
+          borderColor="grey.400"
+          overflow="hidden"
+          position="relative"
+        >
+          <Box zIndex={10} top={0} position="absolute">
+            <Report />
+          </Box>
+          <Map coordinates={coords} />
+          <Box position="absolute" bottom={0}>
+            <Information />
+          </Box>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
+
+export default AddressMapper;
