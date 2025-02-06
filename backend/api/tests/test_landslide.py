4ce12622b82272ae2b4ee790f6e5b70d8a6a5e9a
Anna <annagennadinik@gmail.com>
2025-02-06 12:29:39 -0800

    refactored tests

--- backend/api/tests/test_landslide.py
+++
@@ -1,9 +0,0 @@
-from backend.api.tests.test_session_config import test_engine, test_session, client
-
-
-def test_get_landslide_zones(client):
-    response = client.get(f"/landslide-zones/")
-    response_dict = response.json()
-    print(response_dict["features"])
-    assert response.status_code == 200
-    assert len(response_dict["features"]) == 2
--- backend/api/tests/test_soft_story.py
+++ backend/api/tests/test_soft_story.py
@@ -2,7 +2,7 @@ from backend.api.tests.test_session_config import test_engine, test_session, cli
 
 
 def test_get_soft_stories(client):
-    response = client.get("/soft-stories/")
+    response = client.get("api/soft-stories")
     response_dict = response.json()
     assert response.status_code == 200
     assert len(response_dict["features"]) == 6
@@ -10,14 +10,14 @@ def test_get_soft_stories(client):
 
 def test_is_soft_story(client):
     lon, lat = [-122.424968, 37.76293]
-    response = client.get(f"/soft-stories/is-soft-story?lon={lon}&lat={lat}")
+    response = client.get(f"api/soft-stories/is-soft-story?lon={lon}&lat={lat}")
     assert response.status_code == 200
     assert response.json()  # True
 
     # These should not be soft stories
     wrong_lon, wrong_lat = [0.0, 0.0]
     response = client.get(
-        f"/soft-stories/is-soft-story?lon={wrong_lon}&lat={wrong_lat}"
+        f"api/soft-stories/is-soft-story?lon={wrong_lon}&lat={wrong_lat}"
     )
     assert response.status_code == 200
     assert not response.json()  # False
