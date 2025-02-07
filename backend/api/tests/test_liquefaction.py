b340a2475cf710c2cdb32ff38f98677ab76daa06
agennadi <annagennadinik@gmail.com>
2025-02-06 19:10:33 -0800

    Update backend/api/tests/test_liquefaction.py
    Co-authored-by: Eli Lucherini <elucherini@users.noreply.github.com>

--- backend/api/tests/test_liquefaction.py
+++ backend/api/tests/test_liquefaction.py
@@ -2,7 +2,7 @@ from backend.api.tests.test_session_config import test_engine, test_session, cli
 
 
 def test_get_liquefaction_zones(client):
-    response = client.get(f"api/liquefaction-zones")
+    response = client.get("api/liquefaction-zones")
     response_dict = response.json()
     assert response.status_code == 200
     assert len(response_dict["features"]) == 3
