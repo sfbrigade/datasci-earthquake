563ec1938e5a243d941e9d8cb040333d7a31cf43
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-02-17 14:37:39 -0500

    Removed reinforced_soft_story router and tag

--- backend/api/tags.py
+++ backend/api/tags.py
@@ -6,7 +6,6 @@ from enum import Enum
 class Tags(Enum):
     TSUNAMI = "tsunami"
     SOFT_STORY = "soft story"
-    REINFORCED_SOFT_STORY = "reinforced soft story"
     POLYGONS = "polygons"
     LANDSLIDE = "landslide"
     LIQUEFACTION = "liquefaction"
