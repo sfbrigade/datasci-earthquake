28a836f54a127351ca0bd1a284b43de04621c533
Diya Baliga <baligadiya@gmail.com>
2025-02-19 17:52:27 -0800

    footer - aligned to mapbox

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,21 +1,36 @@
+"use client";
+
 import { Box, HStack, Text } from "@chakra-ui/react";
+import { usePathname } from "next/navigation";
 
 const Footer = () => {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
   return (
     <Box
       as="footer"
       w="100%"
-      p={{
-        base: "8px 23px 8px 23px",
-        md: "14px 26px 14px 26px",
-        xl: "16px 127px 16px 127px",
-      }}
+      position={isHome ? "absolute" : undefined}
+      bottom={isHome ? "0" : undefined}
+      p={
+        isHome
+          ? "7px 7px 6px"
+          : {
+              base: "8px 23px 8px 23px",
+              md: "14px 26px 14px 26px",
+              xl: "16px 127px 16px 127px",
+            }
+      }
     >
-      <HStack justifyContent="space-between">
-        <Text textStyle="textMedium" color="grey.900">
-          © 2024 SF Civic Tech
-        </Text>
-      </HStack>
+      <Text
+        textStyle="textMedium"
+        color="grey.900"
+        align={isHome ? "right" : undefined}
+        m="auto"
+      >
+        © 2024 SF Civic Tech
+      </Text>
     </Box>
   );
 };
