05b96c185c6c59f1d50cd6b663a9b1299a3893ab
Svetlana Eliseeva <mantuok@gmail.com>
2025-03-02 13:58:06 -0800

    moved values retrieve to search bar component

--- app/components/address-mapper.tsx
+++ app/components/address-mapper.tsx
@@ -8,7 +8,6 @@ import Map from "./map";
 import Report from "./report";
 import Information from "./information";
 import { FeatureCollection, Geometry } from "geojson";
-import { mockAddressHazardData } from "../data/data";
 
 const addressLookupCoordinates = {
   geometry: {
@@ -39,22 +38,27 @@ const AddressMapper: React.FC<AddressMapperProps> = ({
     setCoordinates(coords);
   };
 
-  useEffect(() => {
-    if (searchedAddress.length > 0) {
-      console.log("setAddressHazardData");
-      setAddressHazardData(mockAddressHazardData);
-    } else {
-      setAddressHazardData([]);
-    }
-  }, [searchedAddress]);
-
   return (
     <Flex direction="column">
-      <HomeHeader
-        coordinates={coordinates}
-        onSearchChange={updateMap}
-        onAddressSearch={setSearchedAddress}
-      />
+      <Box bgColor="blue">
+        <Box
+          w={{ base: "base", xl: "xl" }}
+          p={{
+            base: "45px 23px 50px 23px",
+            md: "52px 260px 56px 26px",
+            xl: "53px 470px 46px 127px",
+          }}
+          m="auto"
+        >
+          <Heading headingData={headingData} />
+          <SearchBar
+            coordinates={coordinates}
+            onSearchChange={updateMap}
+            onAddressSearch={setSearchedAddress}
+            onCoordDataRetrive={setAddressHazardData}
+          />
+        </Box>
+      </Box>
       <Box w="base" h={{ base: "1400px", md: "1000px" }} m="auto">
         <Box h="100%" overflow="hidden" position="relative">
           <Box zIndex={10} top={0} position="absolute">
