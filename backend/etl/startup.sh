457db472cd14b48c653efa855e2d8440f0140533
Anna <annagennadinik@gmail.com>
2025-03-06 08:41:26 -0800

    time check

--- backend/etl/startup.sh
+++ backend/etl/startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
+echo "Start time: $(date)"
 
 python backend/database/init_db.py
 python backend/etl/liquefaction_data_handler.py
 python backend/etl/soft_story_properties_data_handler.py
 python backend/etl/tsunami_data_handler.py
 
+echo "End time: $(date)"   
 # Start FastAPI
 uvicorn api.index:app --host 0.0.0.0 --port 8000
