9d37e26b245e79546380fce15ddc1a41762359ab
Anna <annagennadinik@gmail.com>
2025-03-11 14:32:19 -0700

    async fastapi in startup.sh

--- backend/etl/startup.sh
+++ backend/etl/startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
+# Start FastAPI
+uvicorn api.index:app --host 0.0.0.0 --port 8000 &
+
 python backend/database/init_db.py
 python backend/etl/liquefaction_data_handler.py
 python backend/etl/soft_story_properties_data_handler.py
 python backend/etl/tsunami_data_handler.py
 
-# Start FastAPI
-uvicorn api.index:app --host 0.0.0.0 --port 8000
