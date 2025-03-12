206e487db310ff973dc5115058b9a784c92ba7e9
Anna <annagennadinik@gmail.com>
2025-03-12 10:54:22 -0700

    Revert "Backend healthcheck (#264)"
    This reverts commit 102bcbb4c5b8d533edbf9c49474a442153bede85.

--- backend/etl/startup.sh
+++ backend/etl/startup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-# Start FastAPI
-uvicorn api.index:app --host 0.0.0.0 --port 8000 &
-
 python backend/database/init_db.py
 python backend/etl/liquefaction_data_handler.py
 python backend/etl/soft_story_properties_data_handler.py
 python backend/etl/tsunami_data_handler.py
 
-# Wait for the FastAPI process (to keep the container alive)
-wait
+# Start FastAPI
+uvicorn api.index:app --host 0.0.0.0 --port 8000
