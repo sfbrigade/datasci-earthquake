102bcbb4c5b8d533edbf9c49474a442153bede85
agennadi <annagennadinik@gmail.com>
2025-03-12 10:45:27 -0700

    Backend healthcheck (#264)
    * Backend healthcheck passes
    
    Co-authored-by: @gamesover
    
    * remove volume
    
    * relative path
    
    * copied public into container
    
    * async fastapi in startup.sh
    
    * removed copying static files
    
    * mounted volume

--- backend/etl/startup.sh
+++ backend/etl/startup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+# Start FastAPI
+uvicorn api.index:app --host 0.0.0.0 --port 8000 &
+
 python backend/database/init_db.py
 python backend/etl/liquefaction_data_handler.py
 python backend/etl/soft_story_properties_data_handler.py
 python backend/etl/tsunami_data_handler.py
 
-# Start FastAPI
-uvicorn api.index:app --host 0.0.0.0 --port 8000
+# Wait for the FastAPI process (to keep the container alive)
+wait
