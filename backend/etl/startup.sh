5f8382411433248c2f4b5b98344e34069dd46646
Anna <annagennadinik@gmail.com>
2025-03-12 15:59:09 -0700

    pruning old data in etl

--- backend/etl/startup.sh
+++ backend/etl/startup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Clear data before running the app
-rm -rf /public/data/*
-
 python backend/database/init_db.py
 python backend/etl/liquefaction_data_handler.py
 python backend/etl/soft_story_properties_data_handler.py
