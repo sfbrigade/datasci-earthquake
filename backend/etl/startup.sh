9d381d35b8d4a1b3a3b38a326ee444a79b15b5d9
Anna <annagennadinik@gmail.com>
2025-04-21 20:58:53 -0700

    store in volume

--- backend/etl/startup.sh
+++ backend/etl/startup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-# Create /public/data if it doesn't exist
-mkdir -p /public/data
-
-# Optionally set permissions (only if needed)
-# Uncomment the lines below if you run into permission issues.
-chown -R appuser:appuser /public/data
-chmod -R 750 /public/data
-
 python backend/database/init_db.py
 python backend/etl/liquefaction_data_handler.py
 python backend/etl/soft_story_properties_data_handler.py
