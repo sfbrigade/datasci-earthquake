9352f443c81d836338a05253755d6e701066e0e8
Svetlana Eliseeva <mantuok@gmail.com>
2025-04-22 22:31:41 -0700

    gemini fixes

--- app/components/address-mapper.tsx
+++ app/components/address-mapper.tsx
@@ -5,7 +5,7 @@ import { Box, Flex, useToast } from "@chakra-ui/react";
 import SearchBar from "./search-bar";
 import Heading, { HeadingProps } from "./heading";
 import Map from "./map";
-import Report from "./report-hazards";
+import ReportHazards from "./report-hazards";
 import Information from "./information";
 import { FeatureCollection, Geometry } from "geojson";
 import HomeHeader from "./home-header";
@@ -99,7 +99,7 @@ const AddressMapper: React.FC<AddressMapperProps> = ({
       <Box w="base" h={{ base: "1400px", md: "1000px" }} m="auto">
         <Box h="100%" overflow="hidden" position="relative">
           <Box zIndex={10} top={0} position="absolute">
-            <Report
+            <ReportHazards
               searchedAddress={searchedAddress}
               addressHazardData={addressHazardData}
               isHazardDataLoading={isHazardDataLoading}
