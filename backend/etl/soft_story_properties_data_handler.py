4d2a8d5611e633cf6c1f376c7fc94820cf9341d8
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-04-26 21:09:53 +0000

    Added initial attempt at address range handling (#308)
    * Added initial attempt at address range handling
    
    * Added test and refactored _addresses_from_range
    
    * Modified duplicate check and soft story model
    
    * Fixed duplicate property addresses
    
    - Added comment to soft story properties model
    
    Co-authored-by: agennadi <annagennadinik@gmail.com>
    
    * Reversed address and property_address
    
    Co-authored-by: agennadi <annagennadinik@gmail.com>
    
    ---------
    
    Co-authored-by: agennadi <annagennadinik@gmail.com>

--- backend/etl/soft_story_properties_data_handler.py
+++ backend/etl/soft_story_properties_data_handler.py
@@ -5,6 +5,7 @@ from backend.api.models.soft_story_properties import SoftStoryProperty
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from dotenv import load_dotenv
 import os
+import re
 from pathlib import Path
 from typing import Dict, Tuple
 from backend.etl.mapbox_geojson_manager import MapboxConfig, MapboxGeojsonManager
@@ -57,7 +58,9 @@ class _SoftStoryPropertiesDataHandler(DataHandler):
                 and data_point["address"] in mapbox_coordinates_map
                 and mapbox_coordinates_map[data_point["address"]]
             ):
-                # mapbox_coordinates_map only contains the addresses that MapBox could resolve, so not all addresses will be there
+                # mapbox_coordinates_map only contains the addresses
+                # that MapBox could resolve, so not all addresses will
+                # be there
                 address = data_point["address"]
                 lon, lat = mapbox_coordinates_map[address]
                 data_point["point"] = f"Point({lon} {lat})"
@@ -91,6 +94,91 @@ class _SoftStoryPropertiesDataHandler(DataHandler):
         self.logger.info(f"There are {len(features)} non-retroffited soft stories")
         return geojson
 
+    @staticmethod
+    def _addresses_from_range(
+        start_range: int, end_range: int, street_name: str
+    ) -> list[str]:
+        """
+        Returns a list of addresses from an address range
+
+        Args:
+            start_range: first address number to include
+            end_range: last address to include
+            street_name: name of the street on which the addresses are
+
+        """
+        # Convert the start and end into a range object
+        number_range = range(start_range, end_range + 1)
+        # Generate the list of addresses
+        addresses = []
+        for number in number_range:
+            addresses.append(f"{number} {street_name}")
+        return addresses
+
+    def _process_feature(
+        self,
+        properties: dict,
+        address: str,
+        sf_geometry: dict,
+        parsed_data: list,
+        addresses: list,
+    ):
+        """
+        Adds the contents of a feature to parsed_data and possibly
+        addresses
+
+        Works by mutating parsed_data and addresses rather than
+        returning a value because the only way to make this function
+        pure would involve duplicatively checking the condition.
+
+        address must be an argument rather than being taken from
+        properties, which has an address field, because the value of
+        that field can have a range of building numbers and therefore
+        instead must be turned into a list of addresses, one per
+        number, by _addresses_from_range and then passed individually
+        to this function.
+        """
+        # Search for the address in the mapbox geojson.
+        # If it's not there, it might be new, so get it
+        # from MapBox freshly.
+        if not self.mapbox_geojson_manager.is_address_in_geojson(address):
+            # Save it for one big later MapBox query
+            addresses.append(address)
+            coordinates = sf_geometry["coordinates"] if sf_geometry else None
+            point_source = "sfdata" if sf_geometry else None
+        else:
+            mapbox_coordinates = self.mapbox_geojson_manager.get_mapbox_coordinates(
+                address
+            )
+            coordinates = (
+                mapbox_coordinates
+                if mapbox_coordinates
+                else sf_geometry["coordinates"] if sf_geometry else None
+            )
+            point_source = (
+                "mapbox" if mapbox_coordinates else "sfdata" if sf_geometry else None
+            )
+
+        parsed_data.append(
+            {
+                "block": properties.get("block"),
+                "lot": properties.get("lot"),
+                "parcel_number": properties.get("parcel_number"),
+                # Property address is just the number and street
+                "property_address": address.split(", SAN FRANCISCO CA")[0],
+                "address": address,
+                "tier": properties.get("tier"),
+                "status": properties.get("status"),
+                "bos_district": properties.get("bos_district"),
+                "point": (
+                    f"Point({coordinates[0]} {coordinates[1]})" if coordinates else None
+                ),
+                "sfdata_as_of": properties.get("data_as_of"),
+                "sfdata_loaded_at": properties.get("data_loaded_at"),
+                "point_source": point_source,
+            }
+        )
+
     def parse_data(self, sf_data: dict) -> tuple[list[dict], dict]:
         """
         Extracts feature attributes and geometry data to construct:
@@ -100,55 +188,31 @@ class _SoftStoryPropertiesDataHandler(DataHandler):
         Geometry data is converted into a GeoAlchemy-compatible
         Point with srid 4326.
         """
-        parsed_data, addresses = [], []
+        parsed_data: list[dict] = []
+        addresses: list[str] = []
+        # Address range of the form '1234-5678 Street Ave'
+        # The pattern seeks digits separated by a hyphen
+        pattern = re.compile(r"(\d+)-(\d+)\s+(.*)")
         for feature in sf_data["features"]:
             properties = feature.get("properties", {})
             sf_geometry = feature.get("geometry", {})
-
-            # Search for the address in the mapbox geojson.
-            # If it's not there, it might be new, so get it
-            # from MapBox freshly.
-            if not self.mapbox_geojson_manager.is_address_in_geojson(
-                properties.get("address")
-            ):
-                # Save it for one big later MapBox query
-                addresses.append(properties.get("address"))
-                coordinates = sf_geometry["coordinates"] if sf_geometry else None
-                point_source = "sfdata" if sf_geometry else None
+            address = properties.get("address")
+            # Tries to find the regular expression in the address
+            match_result = pattern.search(address)
+            if match_result:
+                start_range = int(match_result[1])  # Before the hyphen
+                end_range = int(match_result[2])  # After the hyphen
+                street_name = match_result[3]  # After the range of addreses
+                for address in _SoftStoryPropertiesDataHandler._addresses_from_range(
+                    start_range, end_range, street_name
+                ):
+                    self._process_feature(
+                        properties, address, sf_geometry, parsed_data, addresses
+                    )
             else:
-                mapbox_coordinates = self.mapbox_geojson_manager.get_mapbox_coordinates(
-                    properties.get("address")
+                self._process_feature(
+                    properties, address, sf_geometry, parsed_data, addresses
                 )
-                coordinates = (
-                    mapbox_coordinates
-                    if mapbox_coordinates
-                    else sf_geometry["coordinates"] if sf_geometry else None
-                )
-                point_source = (
-                    "mapbox"
-                    if mapbox_coordinates
-                    else "sfdata" if sf_geometry else None
-                )
-            parsed_data.append(
-                {
-                    "block": properties.get("block"),
-                    "lot": properties.get("lot"),
-                    "parcel_number": properties.get("parcel_number"),
-                    "property_address": properties.get("property_address"),
-                    "address": properties.get("address"),
-                    "tier": properties.get("tier"),
-                    "status": properties.get("status"),
-                    "bos_district": properties.get("bos_district"),
-                    "point": (
-                        f"Point({coordinates[0]} {coordinates[1]})"
-                        if coordinates
-                        else None
-                    ),
-                    "sfdata_as_of": properties.get("data_as_of"),
-                    "sfdata_loaded_at": properties.get("data_loaded_at"),
-                    "point_source": point_source,
-                }
-            )
 
         parsed_data_complete = self.fill_in_missing_mapbox_points(
             parsed_data, addresses
@@ -172,6 +236,6 @@ if __name__ == "__main__":
             soft_story_properties
         )
         handler.save_geojson(soft_story_property_geojson)
-        handler.bulk_insert_data(soft_story_property_objects, "property_address")
+        handler.bulk_insert_data(soft_story_property_objects, "address")
     except HTTPException as e:
         print(f"Failed after retries: {e}")
