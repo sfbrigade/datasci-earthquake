5b66fa0aed417a72939a55a7be77a0b63c6c96c6
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-04-30 20:20:08 -0700

    replace bottom info cards with content below map
    Co-authored-by: AdamFinkle <AdamFinkle@users.noreply.github.com>
    Co-authored-by: Vincent Shuali <100994924+canjalal@users.noreply.github.com>

--- app/components/address-mapper.tsx
+++ app/components/address-mapper.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Box, Flex, useToast } from "@chakra-ui/react";
-import SearchBar from "./search-bar";
-import Heading, { HeadingProps } from "./heading";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  List,
+  ListItem,
+  Image,
+  HStack,
+  Link,
+  useToast,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { HeadingProps } from "./heading";
 import Map from "./map";
 import ReportHazards from "./report-hazards";
-import Information from "./information";
 import { FeatureCollection, Geometry } from "geojson";
 import HomeHeader from "./home-header";
 
@@ -37,7 +47,6 @@ const isErrorResult = (data: unknown): data is ErrorResult => {
 };
 
 const AddressMapper: React.FC<AddressMapperProps> = ({
-  headingData,
   softStoryData,
   tsunamiData,
   liquefactionData,
@@ -111,11 +120,214 @@ const AddressMapper: React.FC<AddressMapperProps> = ({
             tsunamiData={tsunamiData}
             liquefactionData={liquefactionData}
           />
-          <Box position="absolute" bottom={0}>
-            <Information />
-          </Box>
         </Box>
       </Box>
+      <Flex
+        w={{ base: "base", xl: "xl" }}
+        p={{
+          base: "24px 24px 24px 24px",
+          md: "36px 28px 16px 28px",
+          xl: "96px 128px 96px 128px",
+        }}
+        m="auto"
+        gap="46px"
+      >
+        <HStack alignItems={"start"}>
+          <div>
+            <Heading as="h2">
+              <Text
+                as="span"
+                textStyle="headerBig"
+                color="blue"
+                fontWeight="300"
+              >
+                How to be
+                <br />
+                earthquake-ready
+              </Text>
+            </Heading>
+            <Text as="p" mt="2" textStyle="textBig" color="black">
+              Whether you’re a renter, homeowner, or property manager, these
+              resources can help you make confident, informed decisions around
+              earthquake safety.
+            </Text>
+            <Heading as="h3" mt="4">
+              <Text as="span" textStyle="headerMedium">
+                Know the lingo
+              </Text>
+            </Heading>
+            <Text as="p" mt="2">
+              You’ve seen words like “soft story” and “retrofit” floating
+              around, but what do they actually mean?
+            </Text>
+
+            <List textStyle="list" mt="2">
+              <ListItem textStyle="listItem">
+                A{" "}
+                <Text as="span" textStyle="textSemibold">
+                  soft story
+                </Text>{" "}
+                building is a structure that contains an open-floor (or “soft”)
+                level, such as a garage or retail space, below one or more
+                living spaces.
+              </ListItem>
+              <ListItem textStyle="listItem">
+                <Text as="span" textStyle="textSemibold">
+                  Earthquake retrofitting
+                </Text>{" "}
+                is the process of strengthening a building to make it safer in
+                an earthquake, such as adding structural reinforcements or
+                upgrading the foundation to better withstand shaking.
+              </ListItem>
+              <ListItem textStyle="listItem">
+                San Francisco’s{" "}
+                <Text as="span" textStyle="textSemibold">
+                  Mandatory Soft Story Retrofit Ordinance
+                </Text>
+                , enacted in 2013, requires all multi-unit soft story buildings
+                built before 1978 to be retrofitted in order to minimize the
+                risk of earthquake damage.
+              </ListItem>
+            </List>
+
+            <Heading as="h3" mt="4">
+              <Text as="span" textStyle="headerMedium">
+                Plan ahead
+              </Text>
+            </Heading>
+            <Text as="p" mt="2">
+              These quick and easy steps will help you stay safe during future
+              earthquakes.
+            </Text>
+
+            <List textStyle="list" mt="2">
+              <ListItem textStyle="listItem">
+                <Link
+                  as={NextLink}
+                  href="https://www.ready.gov/earthquakes"
+                  textDecoration="underline"
+                >
+                  Learn how to protect yourself
+                </Link>{" "}
+                in an earthquake.
+              </ListItem>
+              <ListItem textStyle="listItem">
+                <Link
+                  as={NextLink}
+                  href="https://www.ready.gov/kit"
+                  textDecoration="underline"
+                >
+                  Prep your emergency kit
+                </Link>{" "}
+                with first aid supplies, batteries, and other essentials.
+              </ListItem>
+              <ListItem textStyle="listItem">
+                <Link
+                  as={NextLink}
+                  href="https://myshake.berkeley.edu/"
+                  textDecoration="underline"
+                >
+                  Download the MyShake app
+                </Link>{" "}
+                to get early warnings when an earthquake is detected.
+              </ListItem>
+            </List>
+
+            <Heading as="h3" mt="4">
+              <Text as="span" textStyle="headerMedium">
+                Find retrofitting services (if applicable)
+              </Text>
+            </Heading>
+            <Text as="p" mt="2">
+              If you own a building in need of retrofitting, the following
+              resources can help you get started.
+            </Text>
+
+            <List textStyle="list" mt="2">
+              <ListItem textStyle="listItem">
+                <Link
+                  as={NextLink}
+                  href="https://www.californiaresidentialmitigationprogram.com/our-seismic-retrofit-programs/the-retrofits/ess-retrofit"
+                  textDecoration="underline"
+                >
+                  Learn more
+                </Link>{" "}
+                about what’s involved in a seismic retrofit.
+              </ListItem>
+              <ListItem textStyle="listItem">
+                <Link
+                  as={NextLink}
+                  href="https://www.californiaresidentialmitigationprogram.com/resources/find-a-contractor/"
+                  textDecoration="underline"
+                >
+                  Find a licensed contractor
+                </Link>{" "}
+                who can perform the right updates to your home.
+              </ListItem>
+              <ListItem textStyle="listItem">
+                Check your eligibility for an{" "}
+                <Link
+                  as={NextLink}
+                  href="https://www.californiaresidentialmitigationprogram.com/our-seismic-retrofit-programs/the-retrofits/ebb-retrofit"
+                  textDecoration="underline"
+                >
+                  Earthquake Brace and Bolt
+                </Link>{" "}
+                grant or{" "}
+                <Link
+                  as={NextLink}
+                  href="https://www.californiaresidentialmitigationprogram.com/our-seismic-retrofit-programs/the-retrofits/ess-retrofit"
+                  textDecoration="underline"
+                >
+                  Earthquake Soft-Story
+                </Link>{" "}
+                retrofit grant.
+              </ListItem>
+            </List>
+
+            <Heading as="h3" mt="4">
+              <Text as="span" textStyle="headerMedium">
+                Know your renters’ rights
+              </Text>
+            </Heading>
+            <Text as="p" mt="2">
+              If you’re a renter in a non-compliant building, liquefaction area,
+              or tsunami zone, you may want to look into these additional
+              resources.
+            </Text>
+            <List textStyle="list" mt="2">
+              <ListItem textStyle="listItem">
+                <Link
+                  as={NextLink}
+                  href="https://www.earthquakeauthority.com/california-earthquake-insurance-policies/renters"
+                  textDecoration="underline"
+                >
+                  Search for renters earthquake insurance
+                </Link>{" "}
+                to cover your personal belongings.
+              </ListItem>
+              <ListItem textStyle="listItem">
+                <Link
+                  as={NextLink}
+                  href="https://sftu.org/repairs/"
+                  textDecoration="underline"
+                >
+                  Learn about your right
+                </Link>{" "}
+                to report unsafe living conditions.
+              </ListItem>
+            </List>
+          </div>
+          <Box flexShrink={0}>
+            <Image
+              src="/images/earthquake-ready.png"
+              alt="about us"
+              width="300"
+              height="300"
+            />
+          </Box>
+        </HStack>
+      </Flex>
     </Flex>
   );
 };
