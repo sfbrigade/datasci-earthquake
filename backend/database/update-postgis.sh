f4e4359379ac35f1120e76beda9d78217c8c2789
agennadi <annagennadinik@gmail.com>
2025-05-22 16:41:46 -0700

    Enabled PRs from forks (#382)
    * Feature/311/enable pr from fork (#379)
    
    * trigger vercel
    
    * Retry Vercel build
    
    * Changed master to main in ci.yml
    
    * Enabled PRs from forks
    
    * Changed main to develop in ci.yml
    
    * Changed master to main in etl_to_neon.yml
    
    * added push to main and db logging
    
    * updated .env example
    
    * omitted postgis version
    
    ---------
    
    Co-authored-by: Anna <annagennadinik@gmail.com>
    
    * use .env for pushes (#380)
    
    * use .env for pushes
    
    * fixed a typo
    
    * updated example env
    
    ---------
    
    Co-authored-by: AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>

--- backend/database/update-postgis.sh
+++ backend/database/update-postgis.sh
@@ -9,20 +9,20 @@ POSTGIS_VERSION="${POSTGIS_VERSION%%+*}"
 
 # Load PostGIS into both template_database and $POSTGRES_DB
 for DB in template_postgis "$POSTGRES_DB" "${@}"; do
-    echo "Updating PostGIS extensions '$DB' to $POSTGIS_VERSION"
+    echo "Ensuring PostGIS extensions in '$DB'"
     psql --dbname="$DB" -c "
         -- Upgrade PostGIS (includes raster)
-        CREATE EXTENSION IF NOT EXISTS postgis VERSION '$POSTGIS_VERSION';
-        ALTER EXTENSION postgis  UPDATE TO '$POSTGIS_VERSION';
+        CREATE EXTENSION IF NOT EXISTS postgis;
+        ALTER EXTENSION postgis UPDATE;
 
         -- Upgrade Topology
-        CREATE EXTENSION IF NOT EXISTS postgis_topology VERSION '$POSTGIS_VERSION';
-        ALTER EXTENSION postgis_topology UPDATE TO '$POSTGIS_VERSION';
+        CREATE EXTENSION IF NOT EXISTS postgis_topology;
+        ALTER EXTENSION postgis_topology UPDATE;
 
         -- Install Tiger dependencies in case not already installed
         CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
         -- Upgrade US Tiger Geocoder
-        CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder VERSION '$POSTGIS_VERSION';
-        ALTER EXTENSION postgis_tiger_geocoder UPDATE TO '$POSTGIS_VERSION';
+        CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+        ALTER EXTENSION postgis_tiger_geocoder UPDATE;
     "
 done
