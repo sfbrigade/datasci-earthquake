e4869ad16e6064264ceac05a79735994ceb55329
Anna <annagennadinik@gmail.com>
2025-06-03 21:08:53 -0700

    debugging

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from backend.api.routers import (
     liquefaction_api,
@@ -6,9 +6,24 @@ from backend.api.routers import (
     soft_story_api,
     health_api,
 )
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/docs", openapi_url="/openapi.json", redirect_slashes=False)
+
+
+# Log origin header for debugging
+@app.middleware("http")
+async def log_origin_header(request: Request, call_next):
+    origin = request.headers.get("origin")
+    logger.info(f"Incoming request from origin: {origin}")
+    return await call_next(request)
+
 
 origins = [
     "https://develop.safehome.report",
@@ -30,3 +45,9 @@ app.include_router(liquefaction_api.router)
 app.include_router(tsunami_api.router)
 app.include_router(soft_story_api.router)
 app.include_router(health_api.router)
+
+
+# Test route
+@app.get("/api/hello")
+async def hello():
+    return {"message": "Hello from FastAPI on Vercel!"}
