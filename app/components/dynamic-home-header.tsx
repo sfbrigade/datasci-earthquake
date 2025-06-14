b8c1da8fbbe913fce3b281c42971c051081eb022
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-06-13 20:40:49 -0700

    get rid of extraneous dynamic components in favor of Suspense

--- app/components/dynamic-home-header.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const DynamicHomeHeader = dynamic(() => import("../components/home-header"), {
-  ssr: false,
-  loading: () => <div>Loading Home Header...</div>,
-});
-
-export default DynamicHomeHeader;
