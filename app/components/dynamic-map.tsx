2b0627b3785258df7a7fc405d7ac568eb3aa6301
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-06-13 20:40:49 -0700

    get rid of extraneous dynamic components in favor of Suspense

--- app/components/dynamic-map.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const DynamicMap = dynamic(() => import("../components/map"), {
-  ssr: false,
-  loading: () => <div>Loading Map...</div>,
-});
-
-export default DynamicMap;
