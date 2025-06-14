1242f84452c9be0b4f7750c26094ce67bd0ab2fa
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-06-13 20:40:49 -0700

    get rid of extraneous dynamic components in favor of Suspense

--- app/components/dynamic-share.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const DynamicShare = dynamic(() => import("../components/share"), {
-  ssr: false,
-  loading: () => <div>Loading Share...</div>,
-});
-
-export default DynamicShare;
