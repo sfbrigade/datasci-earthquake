01979882a7a791aa11a16f2badea8fc37c57428b
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-06-20 22:07:01 -0700

    tweak import order

--- app/components/search-bar.jsx
+++ app/components/search-bar.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useCallback, Suspense } from "react";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import {
   HStack,
