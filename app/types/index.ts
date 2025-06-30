0bae94c26540a02c00d97bed8c19e2c333214742
Jesse Mavi <jessemavi@Jesses-MacBook-Air.local>
2025-06-30 01:19:29 -0500

    Lint fixes.

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,5 +1,5 @@
 export type CoordinateData = {
-  liquefaction: {exists: boolean, last_updated: string | null} | null;
-  softStory: {exists: boolean, last_updated: string | null} | null;
-  tsunami: {exists: boolean, last_updated: string | null} | null;
-}
+  liquefaction: {exists: boolean; last_updated: string | null} | null;
+  softStory: {exists: boolean; last_updated: string | null} | null;
+  tsunami: {exists: boolean; last_updated: string | null} | null;
+};
