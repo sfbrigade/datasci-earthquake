06d2b2276c804625b14e21533597bd42bfb88a80
Josh van Eyken <josh.vaneyken@gmail.com>
2025-07-08 18:29:23 -0700

    install posthog and wrap project with PostHogProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,11 +3,25 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "../styles/theme";
+import { useEffect } from "react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      defaults: "2025-05-24",
+    });
+  }, []);
+
   return (
-    <CacheProvider>
-      <ChakraProvider theme={customTheme}>{children}</ChakraProvider>
-    </CacheProvider>
+    <PostHogProvider client={posthog}>
+      <CacheProvider>
+        <ChakraProvider theme={customTheme}>{children}</ChakraProvider>
+      </CacheProvider>
+    </PostHogProvider>
   );
 }
