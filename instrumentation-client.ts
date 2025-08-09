788e4deba5acb9a477e23e298ca9edf7729a4924
johnwayne.jiang <johnwayne.jiang2024@gmail.com>
2025-08-08 18:59:00 -0700

    set the capture_pageview as false

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,7 +4,7 @@ if (typeof window !== "undefined") {
   posthog.init("phc_i31362DEdeHty6QpUfc5kdpAkwgxTLu5dppb2eALWQ7", {
     api_host: "https://us.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-    capture_pageview: true, // enable automatic pageview capture, as we capture manually
+    capture_pageview: false, // enable automatic pageview capture, as we capture manually
   });
 }
 
