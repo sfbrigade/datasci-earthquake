f81d67776d7eb4eb095528db011e0ab9702a9043
johnwayne.jiang <johnwayne.jiang2024@gmail.com>
2025-08-19 12:34:38 -0700

    fix the uv virtual environment

--- backend/etl/startup.sh
+++ backend/etl/startup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -e
 
-python backend/database/init_db.py
-python backend/etl/liquefaction_data_handler.py
-python backend/etl/soft_story_properties_data_handler.py
-python backend/etl/tsunami_data_handler.py
+# uv automatically creates a virtual environment called .venv in the backend Dockerfile
+# when we run uv sync there. In order to use .venv and all the packages we have installed
+# into it via this backend Dockerfile, we must specify the path to the python interpreter
+# inside .venv
+VENV_PYTHON="/backend/.venv/bin/python"
+
+# We then use this virtual environment python, rather than the system python
+$VENV_PYTHON backend/database/init_db.py
+$VENV_PYTHON backend/etl/liquefaction_data_handler.py
+$VENV_PYTHON backend/etl/soft_story_properties_data_handler.py
+$VENV_PYTHON backend/etl/tsunami_data_handler.py
 
 # Start FastAPI
-uvicorn api.index:app --host 0.0.0.0 --port 8000 
+$VENV_PYTHON -m uvicorn api.index:app --host 0.0.0.0 --port 8000
