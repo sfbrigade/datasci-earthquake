faf24cdbb5b1a6dd09b335b2716be17637f37086
Jesse Mavi <jessemavi@Jesses-MacBook-Air.local>
2025-08-20 00:02:19 -0500

    Try adding transformIgnorePatterns key with chakra as value to jest config.

--- jest.config.ts
+++ jest.config.ts
@@ -16,6 +16,9 @@ const config: Config = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/app/$1",
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@chakra-ui|@emotion)/",
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
