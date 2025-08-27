3756ab6f142b577f73605c757f477c9da9cb0e3b
AdamFinkle <77808710+AdamFinkle@users.noreply.github.com>
2025-08-27 14:54:57 -0400

    Added bullet point format
    Co-authored-by: jjiang10 <18101636214@163.com>

--- app/alert-links/page.tsx
+++ app/alert-links/page.tsx
@@ -1,4 +1,4 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text, List } from "@chakra-ui/react";
 import Link from "next/link";
 
 const AlertLinks = () => {
@@ -15,14 +15,18 @@ const AlertLinks = () => {
           Earthquake alerts to sign up for:
         </Text>
       </Heading>
-      <Link
-        href="https://myshake.berkeley.edu/"
-        style={{ textDecoration: "underline" }}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Download the MyShake app
-      </Link>
+      <List.Root textStyle="textMedium" layerStyle="list">
+        <List.Item>
+          <Link
+            href="https://myshake.berkeley.edu/"
+            style={{ textDecoration: "underline" }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download the MyShake app
+          </Link>
+        </List.Item>
+      </List.Root>
     </div>
   );
 };
