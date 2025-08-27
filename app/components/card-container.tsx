a607e40cda9fb07d046a8123439dd51189b1a1c0
Jobe <eloh2012@gmail.com>
2025-08-27 14:05:26 -0400

    clean up

--- app/components/card-container.tsx
+++ app/components/card-container.tsx
@@ -3,20 +3,13 @@ import { Box, Stack } from "@chakra-ui/react";
 
 export const CardContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    // previously was Center rather than Box and w="100vw"
     <Box
       pt="16px"
       px={{ base: "24px", md: "28px", xl: "32px" }}
       zIndex={10}
       w="full"
     >
-      <Stack
-        // previously had px={{ base: "24px", md: "28px", xl: "128px" }} and direction={{ base: "column", md: "row" }}
-        // gap was "16px"
-        gap="14px"
-      >
-        {children}
-      </Stack>
+      <Stack gap="14px">{children}</Stack>
     </Box>
   );
 };
