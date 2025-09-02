e12f19c764c309781be1d580a118507cb7e3d7a1
Jesse Mavi <jessemavi@Jesses-MacBook-Air.local>
2025-09-02 01:07:17 -0500

    Set fallback to null in Suspense wrapping AddressMapper. Remove Suspense wrapping around SearchBar.

--- app/components/home-header.tsx
+++ app/components/home-header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Headings } from "../data/data";
 import { Box, Stack, Text } from "@chakra-ui/react";
@@ -8,7 +8,6 @@ import { AnimatePresence, motion } from "framer-motion";
 import Heading from "./heading";
 import ReportAddress from "./report-address";
 import SearchBar from "./search-bar";
-import SearchBarSkeleton from "./search-bar-skeleton";
 import Share from "./share";
 import ShareSkeleton from "./share-skeleton";
 // import { ColorModeButton } from "../components/ui/color-mode";
@@ -83,10 +82,7 @@ const HomeHeader = ({
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
     >
-      {/* NOTE: This Suspense boundary is being used around a component that utilizes `useSearchParams()` to prevent entire page from deopting into client-side rendering (CSR) bailout as per https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
-      <Suspense fallback={<SearchBarSkeleton />}>
-        <SearchBar onSearchChange={onSearchChange} />
-      </Suspense>
+      <SearchBar onSearchChange={onSearchChange} />
     </motion.div>
   );
 
--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,8 @@ const Home = async () => {
   }
   return (
     <Flex direction="column">
-      <Suspense>
+      {/* NOTE: This Suspense boundary is being used around a component that utilizes `useSearchParams()` to prevent entire page from deopting into client-side rendering (CSR) bailout as per https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
+      <Suspense fallback={null}>
         <AddressMapper
           softStoryData={softStoryData}
           tsunamiData={tsunamiData}
