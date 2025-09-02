a3653fc1b0b3bd539623a576d093c54c404551a8
Josh van Eyken <josh.vaneyken@gmail.com>
2025-09-01 19:35:28 -0700

    Refactor ReportAddress component

--- app/components/report-address.tsx
+++ app/components/report-address.tsx
@@ -7,18 +7,30 @@ interface ReportAddressProps {
 const ReportAddress: React.FC<ReportAddressProps> = ({ searchedAddress }) => {
   return (
     <Stack
+      // border={"1px solid pink"}
       // gap={"9px"}
-      direction={{ base: "row", md: "row" }}
+      direction={{ base: "row", xl: "row" }}
       alignItems={{ base: "flex-start", md: "center" }}
-      gap={{ base: 0, md: 1 }} // TODO FIXME: double check if this should be "9px", as commented out above
+      gap={{ base: 1, md: 1 }} // TODO FIXME: double check if this should be "9px", as commented out above
     >
-      <Text textStyle="headerReport" layerStyle="headerMain" fontSize={{ base: "xs"}}>
-        Report for
+      <Text
+        textStyle="headerReport"
+        layerStyle="headerMain"
+        fontSize="3xl"
+        // fontWeight={{ base: "bold" }}
+        // border={"1px solid orange"}
+        // p={"0"}
+      >
+        Report for {searchedAddress}
       </Text>
-     
-      <Text textStyle="headerReport" layerStyle="headerMain" fontSize={{ base: "xs"}}>
+      {/* <Text
+        textStyle="headerReport"
+        layerStyle="headerMain"
+        fontSize={{ base: "2xl" }}
+        // border={"1px solid lightblue"}
+      >
         {searchedAddress}
-      </Text>
+      </Text> */}
     </Stack>
   );
 };
