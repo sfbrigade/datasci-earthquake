e2092223a7e8cff07bd73ca6dd77530d3c165145
Jobe <eloh2012@gmail.com>
2025-09-22 16:12:04 -0400

    Reverted refactoring of page.tsx file's content

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ import {
   fetchTsunami,
   fetchLiquefaction,
 } from "./api/services";
-import { HomePageText } from "./data/data";
 
 // NOTE: UI changes to this page ought to be reflected in its suspense skeleton `home-skeleton.tsx` and vice versa
 // TODO: look into if we can use narrow Suspense boundaries instead of `loading.tsx` and achieve the same (or better) perceived loading time effect
@@ -78,114 +77,181 @@ const Home = async () => {
 
             <Heading as="h3" mt="7">
               <Text as="span" textStyle="headerMedium" layerStyle="headerAlt">
-                {HomePageText[0].title}
+                Know the lingo
               </Text>
             </Heading>
             <Text as="p" mt="2">
-              {HomePageText[0].subtext}
+              You’ve seen words like “soft story” and “retrofit” floating
+              around, but what do they actually mean?
             </Text>
+
             <List.Root textStyle="textMedium" layerStyle="list" mt="0">
-              {HomePageText[0].listItems.map((li, index) => (
-                <List.Item key={index}>{li}</List.Item>
-              ))}
+              <List.Item>
+                A soft story building is a structure that contains an open-floor
+                (or “soft”) level, such as a garage or retail space, below one
+                or more living spaces.
+              </List.Item>
+              <List.Item>
+                Earthquake retrofitting is the process of strengthening a
+                building to make it safer in an earthquake, such as adding
+                structural reinforcements or upgrading the foundation to better
+                withstand shaking.
+              </List.Item>
+              <List.Item>
+                San Francisco’s Mandatory Soft Story Retrofit Ordinance ,
+                enacted in 2013, requires all multi-unit soft story buildings
+                built before 1978 to be retrofitted in order to minimize the
+                risk of earthquake damage. Soft-story homes with 1 to 4 units
+                are still vulnerable to earthquake damage, even though the
+                ordinance does not legally require them to be retrofitted.
+              </List.Item>
             </List.Root>
 
-            {HomePageText.slice(1).map((data, index) =>
-              data.links !== undefined && data.links.length < 4 ? (
-                <div key={index}>
-                  <Heading as="h3" mt="7">
-                    <Text
-                      as="span"
-                      textStyle="headerMedium"
-                      layerStyle="headerAlt"
-                    >
-                      {data.title}
-                    </Text>
-                  </Heading>
-                  <Text as="p" mt="1">
-                    {data.subtext}
-                  </Text>
+            <Heading as="h3" mt="7">
+              <Text as="span" textStyle="headerMedium" layerStyle="headerAlt">
+                Plan ahead
+              </Text>
+            </Heading>
+            <Text as="p" mt="2">
+              These quick and easy steps will help you stay safe during future
+              earthquakes.
+            </Text>
 
-                  <List.Root textStyle="textMedium" layerStyle="list" mt="0">
-                    {data.listItems.map((li, index) => (
-                      <List.Item key={index} mt="1">
-                        <Link
-                          as={NextLink}
-                          href={data.links[index]}
-                          textDecoration="underline"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {data.linkText[index]}
-                        </Link>{" "}
-                        {li}
-                      </List.Item>
-                    ))}
-                  </List.Root>
-                </div>
-              ) : (
-                <div key={index}>
-                  <Heading as="h3" mt="7">
-                    <Text
-                      as="span"
-                      textStyle="headerMedium"
-                      layerStyle="headerAlt"
-                    >
-                      {data.title}
-                    </Text>
-                  </Heading>
-                  <Text as="p" mt="1">
-                    {data.subtext}
-                  </Text>
+            <List.Root textStyle="textMedium" layerStyle="list" mt="0">
+              <List.Item>
+                <Link
+                  as={NextLink}
+                  href="https://www.ready.gov/earthquakes"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn how to protect yourself
+                </Link>{" "}
+                in an earthquake.
+              </List.Item>
+              <List.Item>
+                <Link
+                  as={NextLink}
+                  href="https://www.ready.gov/kit"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Prep your emergency kit
+                </Link>{" "}
+                with first aid supplies, batteries, and other essentials.
+              </List.Item>
+              <List.Item>
+                <Link
+                  as={NextLink}
+                  href="https://myshake.berkeley.edu/"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Download the MyShake app
+                </Link>{" "}
+                to get early warnings when an earthquake is detected.
+              </List.Item>
+            </List.Root>
 
-                  <List.Root textStyle="textMedium" layerStyle="list" mt="0">
-                    {data.listItems.slice(0, 2).map((li, index) => (
-                      <List.Item key={index} mt="1">
-                        <Link
-                          as={NextLink}
-                          href={
-                            data.links !== undefined ? data.links[index] : ""
-                          }
-                          textDecoration="underline"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {data.linkText !== undefined
-                            ? data.linkText[index]
-                            : ""}
-                        </Link>{" "}
-                        {li}
-                      </List.Item>
-                    ))}
-                    <List.Item mt="1">
-                      {data.listItems[2]}{" "}
-                      <Link
-                        as={NextLink}
-                        href={data.links !== undefined ? data.links[2] : ""}
-                        textDecoration="underline"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {data.linkText !== undefined ? data.linkText[2] : ""}
-                      </Link>{" "}
-                      {data.listItems[3]}{" "}
-                      <Link
-                        as={NextLink}
-                        href={data.links !== undefined ? data.links[3] : ""}
-                        textDecoration="underline"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {data.linkText !== undefined
-                          ? data.linkText[3]
-                          : ""}{" "}
-                      </Link>{" "}
-                      {data.listItems[4]}
-                    </List.Item>
-                  </List.Root>
-                </div>
-              )
-            )}
+            <Heading as="h3" mt="7">
+              <Text as="span" textStyle="headerMedium" layerStyle="headerAlt">
+                Find retrofitting services (if applicable)
+              </Text>
+            </Heading>
+            <Text as="p" mt="2">
+              If you own a building in need of retrofitting, the following
+              resources can help you get started.
+            </Text>
+
+            <List.Root textStyle="textMedium" layerStyle="list" mt="0">
+              <List.Item>
+                <Link
+                  as={NextLink}
+                  href="https://www.californiaresidentialmitigationprogram.com/our-seismic-retrofit-programs/the-retrofits/ess-retrofit"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn more
+                </Link>{" "}
+                about what’s involved in a seismic retrofit.
+              </List.Item>
+              <List.Item>
+                <Link
+                  as={NextLink}
+                  href="https://www.californiaresidentialmitigationprogram.com/resources/find-a-contractor/"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Find a licensed contractor
+                </Link>{" "}
+                who can perform the right updates to your home.
+              </List.Item>
+              <List.Item>
+                Check your eligibility for an{" "}
+                <Link
+                  as={NextLink}
+                  href="https://www.californiaresidentialmitigationprogram.com/our-seismic-retrofit-programs/the-retrofits/ebb-retrofit"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Earthquake Brace and Bolt
+                </Link>{" "}
+                grant or{" "}
+                <Link
+                  as={NextLink}
+                  href="https://www.californiaresidentialmitigationprogram.com/our-seismic-retrofit-programs/the-retrofits/ess-retrofit"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Earthquake Soft-Story
+                </Link>{" "}
+                retrofit grant.
+              </List.Item>
+            </List.Root>
+
+            <Heading as="h3" mt="7">
+              <Text as="span" textStyle="headerMedium" layerStyle="headerAlt">
+                Know your renters’ rights
+              </Text>
+            </Heading>
+            <Text as="p" mt="2">
+              If you’re a renter in a non-compliant building, liquefaction area,
+              or tsunami zone, you may want to look into these additional
+              resources.
+            </Text>
+            <List.Root textStyle="textMedium" layerStyle="list" mt="0">
+              <List.Item>
+                <Link
+                  as={NextLink}
+                  href="https://www.earthquakeauthority.com/california-earthquake-insurance-policies/renters"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Search for renters earthquake insurance
+                </Link>{" "}
+                to cover your personal belongings.
+              </List.Item>
+              <List.Item>
+                <Link
+                  as={NextLink}
+                  href="https://sftu.org/repairs/"
+                  textDecoration="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn about your right
+                </Link>{" "}
+                to report unsafe living conditions.
+              </List.Item>
+            </List.Root>
           </div>
         </HStack>
         <Box flexShrink={0} display={{ base: "none", lg: "block" }}>
