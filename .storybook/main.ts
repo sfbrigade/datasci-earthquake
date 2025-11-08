27ea86b56f3081b6d3fe76aa7756150abb28b562
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-11-07 21:09:23 -0800

    add welcome page and reorder stories more logically

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,18 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
+  // NOTE: more advanced sort is possible if needed: https://storybook.js.org/docs/writing-stories/naming-components-and-hierarchy
   stories: [
+    "../stories/Welcome.mdx",
+    // components first
     "../app/components/**/*.mdx",
     "../app/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    // tokens next
+    "../stories/*.mdx",
+    "../stories/*.stories.@(js|jsx|mjs|ts|tsx)",
+    // subfolders last (including examples)
+    "../stories/*/**/*.mdx",
+    "../stories/*/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@chromatic-com/storybook",
