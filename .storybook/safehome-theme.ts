d1fe1a137935ff69a7d14384bf2f0eae925d58ed
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-11-07 21:50:46 -0800

    start adding SafeHome branding to Storybook

---
+++ .storybook/safehome-theme.ts
@@ -0,0 +1,9 @@
+import { create } from "storybook/theming";
+
+export default create({
+  base: "dark",
+  brandTitle: "SafeHome Storybook",
+  brandUrl: "https://safehome.report",
+  brandImage: "../public/images/SFSafeHome-fulllogo.svg",
+  brandTarget: "_self",
+});
