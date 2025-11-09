2c9ef6e2cdec71b12b78633e2658f788e9c84356
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-11-08 16:50:07 -0800

    attempt to fix build error using LLM suggestion

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-
-import { next } from "eslint-config-next";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-
-import prettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+
+// Conditionally import Next.js configs, handling possible missing types.
+let next: any = [];
+let nextCoreWebVitals: any = [];
+
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  next = require("eslint-config-next");
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  nextCoreWebVitals = require("eslint-config-next/core-web-vitals");
+} catch (e) {
+  // If Next.js config packages are not installed, fallback to empty configs.
+  next = [];
+  nextCoreWebVitals = [];
+}
+
+import { globalIgnores } from "eslint/config";
 
 import { FlatCompat } from "@eslint/eslintrc";
 
