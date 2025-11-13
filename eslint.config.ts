5b5a5d3b3fa72bfa40a46edffcf96c5d9f625691
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2025-11-12 16:23:47 -0800

    fix broken export

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-plugin-prettier";
+// FIXME: figure out how to properly import the following without errors
 // Add type declarations for modules without types
 // // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // // @ts-expect-error: No types for eslint-config-next
