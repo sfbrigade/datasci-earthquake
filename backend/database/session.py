1eeb75698042f04658698d08bdca5221b89dda92
Newton Chung <newtonchung99@gmail.com>
2026-01-09 01:18:15 -0800

    Reconfigured Database Initialization to use Lazy Loading When the backend starts up, top-level code is run immediately, before Railway is able to inject env vars. This defers the creation of objects until they're needed.

--- backend/database/session.py
+++ backend/database/session.py
@@ -2,6 +2,7 @@ import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from backend.api.config import get_settings
+from functools import lru_cache
 
 
 def _get_database_url() -> str:
@@ -10,7 +11,7 @@ def _get_database_url() -> str:
         case "local" | "ci":
             return settings.localhost_database_url_sqlalchemy
         case "prod":
-            return settings.neon_url
+            return settings.database_url_sqlalchemy
         case "dev_docker":
             return settings.database_url_sqlalchemy
         case _:
@@ -18,24 +19,27 @@ def _get_database_url() -> str:
 
 
 # Set up the database engine using settings
-engine = create_engine(
-    _get_database_url(),
-    pool_size=10,
-    pool_timeout=30,
-    max_overflow=5,
-    pool_pre_ping=True,
-    pool_recycle=3600,
-)
+@lru_cache(maxsize=1)
+def get_engine():
+    engine = create_engine(
+        _get_database_url(),
+        pool_size=10,
+        pool_timeout=30,
+        max_overflow=5,
+        pool_pre_ping=True,
+        pool_recycle=3600,
+    )
+    logging.getLogger("sqlalchemy.pool").setLevel(logging.WARNING)
+    return engine
 
-logging.getLogger("sqlalchemy.pool").setLevel(logging.WARNING)
 
 # Create a session factory
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False)
 
 
 # Dependency function to get a database session
 def get_db():
-    db = SessionLocal()
+    db = SessionLocal(bind=get_engine())
     try:
         yield db
     finally:
