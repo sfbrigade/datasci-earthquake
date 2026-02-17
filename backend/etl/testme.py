75773f829140b7b344ada80e542784100e65567c
zztop <254842763+bees-dont-have-knees@users.noreply.github.com>
2026-02-16 21:00:39 -0500

    more testing

--- backend/etl/testme.py
+++ backend/etl/testme.py
@@ -3,7 +3,7 @@ import os
 if __name__ == "__main__":
   print(os.getenv('ENVIRONMENT', 'hahaoops'))
   if (os.getenv('NEON_URL')):
-    i = os.getenv('NEON_URL').lower().index('https')
+    i = os.getenv('NEON_URL').lower().index('dummy')
     print(f'i = {i}')
   else:
     print('neon doesnt exist')
