c2dd6b82c07147c64e33f5a77c61eb8bb066491c
Josh van Eyken <josh.vaneyken@gmail.com>
2026-02-24 17:51:42 -0800

    Clean up HomeHeader and prevent search bar appearing away from home page

--- app/components/home-header.tsx
+++ app/components/home-header.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { Suspense, useCallback, useContext } from "react";
+import { Suspense, useCallback } from "react";
 import { useAtomValue } from "jotai";
 import {
   searchedAddressAtom,
   isSearchCompleteAtom,
 } from "@/atoms/AddressSearchAtom";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { Headings } from "../data/data";
-// import { AddressSearchContext } from "./address-mapper";
+
 import {
   Box,
   Text,
@@ -18,7 +18,7 @@ import {
   Link,
   Flex,
 } from "@chakra-ui/react";
-import Heading, { HeadingProps } from "./heading";
+import Heading from "./heading";
 import ReportAddress from "./report-address";
 import SearchBar from "./search-bar";
 import Share from "./share";
@@ -30,15 +30,9 @@ export type HazardData = {
   tsunami: { exists: boolean; last_updated: string | null } | null;
 };
 
-// interface HomeHeaderProps {
-//   searchedAddress: string | null;
-//   isSearchComplete: boolean;
-//   // onSearchChange: (coords: number[], address: string) => void;
-// }
-
 const HomeHeader = () => {
-  // const { searchedAddress, isSearchComplete } =
-  //   useContext(AddressSearchContext);
+  const pathname = usePathname();
+  const isHome = pathname === "/";
   const searchedAddress = useAtomValue(searchedAddressAtom);
   const isSearchComplete = useAtomValue(isSearchCompleteAtom);
 
@@ -54,7 +48,13 @@ const HomeHeader = () => {
   );
 
   return (
-    <Box as="header" bgGradient="blue" py={{ base: "4", "2xl": "5" }} px="8">
+    <Box
+      as="header"
+      bgGradient="blue"
+      py={{ base: "4", "2xl": "5" }}
+      px="8"
+      w="full"
+    >
       <Flex
         direction={{
           base: "column",
@@ -93,139 +93,33 @@ const HomeHeader = () => {
             Beta
           </Text>
         </HStack>
-        {isSearchComplete ? (
+        {isSearchComplete && isHome ? (
           <ReportAddress searchedAddress={searchedAddress} />
         ) : (
           <Heading headingData={headingData} />
         )}
       </Flex>
 
-      <Flex
-        direction={{ base: "column-reverse", xl: "row" }}
-        justifyContent={"space-between"}
-        alignItems={{ base: "flex-start", xl: "center" }}
-      >
-        <Box width={{ base: "full", xl: "fit" }}>
-          <SearchBar onSearchChange={handleSearchChange} />
-        </Box>
+      {isHome ? (
+        <Flex
+          direction={{ base: "column-reverse", xl: "row" }}
+          justifyContent={"space-between"}
+          alignItems={{ base: "flex-start", xl: "center" }}
+        >
+          <Box width={{ base: "full", xl: "fit" }}>
+            <SearchBar onSearchChange={handleSearchChange} />
+          </Box>
 
-        {/* NOTE: This Suspense boundary is being used around a component that utilizes `useSearchParams()` to prevent entire page from deopting into client-side rendering (CSR) bailout as per https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
-        {isSearchComplete ? (
-          <Suspense fallback={<ShareSkeleton />}>
-            <Share />
-          </Suspense>
-        ) : null}
-      </Flex>
+          {/* NOTE: This Suspense boundary is being used around a component that utilizes `useSearchParams()` to prevent entire page from deopting into client-side rendering (CSR) bailout as per https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
+          {isSearchComplete ? (
+            <Suspense fallback={<ShareSkeleton />}>
+              <Share />
+            </Suspense>
+          ) : null}
+        </Flex>
+      ) : null}
     </Box>
   );
 };
 
 export default HomeHeader;
-// "use client";
-
-// import { Suspense } from "react";
-// import { useRouter } from "next/navigation";
-// import { Headings } from "../data/data";
-// import {
-//   Box,
-//   Text,
-//   HStack,
-//   Image,
-//   VisuallyHidden,
-//   Link,
-//   Flex,
-// } from "@chakra-ui/react";
-// import Heading, { HeadingProps } from "./heading";
-// import ReportAddress from "./report-address";
-// import SearchBar from "./search-bar";
-// import Share from "./share";
-// import ShareSkeleton from "./share-skeleton";
-
-// export type HazardData = {
-//   liquefaction: { exists: boolean; last_updated: string | null } | null;
-//   softStory: { exists: boolean; last_updated: string | null } | null;
-//   tsunami: { exists: boolean; last_updated: string | null } | null;
-// };
-
-// interface HomeHeaderProps {
-//   searchedAddress: string | null;
-//   isSearchComplete: boolean;
-//   onSearchChange: (coords: number[], address: string) => void;
-// }
-
-// const HomeHeader = ({
-//   searchedAddress,
-//   isSearchComplete,
-//   onSearchChange,
-// }: HomeHeaderProps) => {
-//   const headingData = Headings.home;
-//   const router = useRouter();
-
-//   return (
-//     <Box as="header" bgGradient="blue" py={{ base: "4", "2xl": "5" }} px="8">
-//       <Flex
-//         direction={{
-//           base: "column",
-//           md: "column",
-//           lg: "row-reverse",
-//         }}
-//         justifyContent={"space-between"}
-//         alignItems={{ base: "flex-start", xl: "center" }}
-//         gap="1.5"
-//         mb={{ base: "2" }}
-//       >
-//         <HStack align="start" gap="1">
-//           <Link
-//             as={"a"}
-//             color="white"
-//             href="/"
-//             cursor="button"
-//             textDecoration={"none"}
-//             onClick={(e) => {
-//               e.preventDefault();
-//               router.push("/");
-//             }}
-//           >
-//             <HStack align="baseline">
-//               <Image
-//                 src="/images/SFSafeHome-fulllogo.svg"
-//                 alt="SafeHome logo"
-//                 role="img" // needed for VoiceOver bug: https://bugs.webkit.org/show_bug.cgi?id=216364
-//                 height="safeHomeLogoHeight"
-//                 width="safeHomeLogoWidth"
-//               />
-//               <VisuallyHidden>SafeHome</VisuallyHidden>
-//             </HStack>{" "}
-//           </Link>
-//           <Text textStyle="textPrerelease" layerStyle="prerelease">
-//             Beta
-//           </Text>
-//         </HStack>
-//         {isSearchComplete ? (
-//           <ReportAddress searchedAddress={searchedAddress} />
-//         ) : (
-//           <Heading headingData={headingData} />
-//         )}
-//       </Flex>
-
-//       <Flex
-//         direction={{ base: "column-reverse", xl: "row" }}
-//         justifyContent={"space-between"}
-//         alignItems={{ base: "flex-start", xl: "center" }}
-//       >
-//         <Box width={{ base: "full", xl: "fit" }}>
-//           <SearchBar onSearchChange={onSearchChange} />
-//         </Box>
-
-//         {/* NOTE: This Suspense boundary is being used around a component that utilizes `useSearchParams()` to prevent entire page from deopting into client-side rendering (CSR) bailout as per https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
-//         {isSearchComplete ? (
-//           <Suspense fallback={<ShareSkeleton />}>
-//             <Share />
-//           </Suspense>
-//         ) : null}
-//       </Flex>
-//     </Box>
-//   );
-// };
-
-// export default HomeHeader;
