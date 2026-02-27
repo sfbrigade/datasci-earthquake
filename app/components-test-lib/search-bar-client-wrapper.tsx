f1083160ccc63b82453dcd0eb71282c6e26b2e74
Josh van Eyken <josh.vaneyken@gmail.com>
2026-02-26 21:28:02 -0800

    Changes to components-test-lib

--- app/components-test-lib/search-bar-client-wrapper.tsx
+++ app/components-test-lib/search-bar-client-wrapper.tsx
@@ -3,6 +3,12 @@
 import SearchBar from "../components/search-bar";
 
 // NOTE: SearchBar resides here instead of directly in the components test library because event handlers are not allowed to be passed into a Client Component from a Server Component
-const SearchBarClientWrapper = ({}) => <SearchBar onSearchChange={() => {}} />;
+const SearchBarClientWrapper = ({}) => (
+  <SearchBar
+    inputAddress=""
+    onInputAddressChange={() => {}}
+    onSearchChange={() => {}}
+  />
+);
 
 export default SearchBarClientWrapper;
