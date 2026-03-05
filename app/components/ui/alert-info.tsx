e5590bbbae29ac242963a5136e1cdad4c61e25de
nutanp <nutan.pandeya@gmail.com>
2026-03-04 17:10:13 -0800

    WIP-Code for red alert message on map - move alert info onto map - fix color palette for red - also remove extraneous showHazards state (can be done in commit that stops using MobileReportHazards)

--- app/components/ui/alert-info.tsx
+++ app/components/ui/alert-info.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Alert } from "@chakra-ui/react";
-import { LuAlarmClockPlus } from "react-icons/lu";
+import { IoIosWarning } from "react-icons/io";
 
 interface AlertInfoProps {
   message: string;
@@ -10,7 +10,7 @@ const AlertInfo: React.FC<AlertInfoProps> = ({ message }) => (
   <div>
     <Alert.Root status="error">
       <Alert.Indicator>
-        <LuAlarmClockPlus />
+        <IoIosWarning />
       </Alert.Indicator>
       <Alert.Title>{message}</Alert.Title>
     </Alert.Root>
