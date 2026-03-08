fcf258959633b82b1883e9018e2ab56852daacca
zztop <254842763+bees-dont-have-knees@users.noreply.github.com>
2026-03-08 15:44:13 -0400

    test

--- stuff.cjs
+++ stuff.cjs
@@ -1,45 +1,21 @@
 const cacache = require('cacache')
 
+function doThis() {
+  console.log('all done...')
+
+  cacache.ls('/app/drinksonyou').then(console.log)
+}
+
+
 cacache.ls('/home/node/.npm/_cacache').then((value) => {
-
   for (const [key, v] of Object.entries(value)) {
-    console.log(`my key rules: ${key}`)
-
     cacache.get('/home/node/.npm/_cacache', key).then((obj) => {
       cacache.put('/app/drinksonyou', key, obj.data).then(integrity => {
-        //console.log('integrity hash is ', integrity)
-        //console.log(`now try retrieving ${key}....`)
         cacache.get.info('/app/drinksonyou', key).then((o) => {
-          console.log('getting the diggy...')
-          console.log(typeof o)
           console.log(o)
-          console.log(o.path)
         })
       })
-      /** 
-      Object.keys(obj).forEach((theKey, index) => {
-        console.log(theKey)
-      })
-      **/
-      //console.log(obj.data instanceof Buffer)
     })
   }
+}).then(doThis)
 
-  //console.log(Object.entries(value))
-  //const haha = 'make-fetch-happen:request-cache:https://registry.npmjs.org/memorystream/-/memorystream-0.3.1.tgz'
-  //cacache.get('/home/node/.npm/_cacache', haha).then(console.log)
-  //Object.keys(value).forEach((item, index) => {
-    //cacache.put('/home/node/mystuff', item)
-  //})
-})
-
-//cacache.ls('/app/drinksonyou').then(console.log)
-/***
-cacache.get.info('/app/drinksonyou', theKeyMaster).then(console.log)
-const haha = 'make-fetch-happen:request-cache:https://registry.npmjs.org/memorystream/-/memorystream-0.3.1.tgz'
-const oh = 'make-fetch-happen:request-cache:https://registry.npmjs.org/nice-try/-/nice-try-1.0.5.tgz'
-cacache.get('/app/drinksonyou', haha).then((obj) => {
-  console.log('go here...')
-  console.log(obj.data instanceof Buffer)
-})
-**/
