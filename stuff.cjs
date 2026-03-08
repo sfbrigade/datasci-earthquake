90d1c2755ace11edfedb5c701538db205620e105
zztop <254842763+bees-dont-have-knees@users.noreply.github.com>
2026-03-08 11:10:20 -0400

    test

--- stuff.cjs
+++ stuff.cjs
@@ -1,28 +1,22 @@
 const cacache = require('cacache')
 
-function replacer(key, value) {
-  console.log(key)
-}
-
 cacache.ls('/home/node/.npm/_cacache').then((value) => {
-  // JSON.stringify(value, replacer)
-  console.log(typeof value)
-  /**
+
   for (const [key, v] of Object.entries(value)) {
-    console.log(`my key: ${key}`)
-    console.log(Object.entries(v))
-    
-    for (const [morekey, morev] of Object.entries(v)) {
-      console.log(`- more key: ${morekey}`)
-    }
-    console.log('----------------')
+    console.log(`my key rules: ${key}`)
+
+    cacache.get('/home/node/.npm/_cacache', key).then((myv) => {
+      cacache.put('/home/mystuffycache', key, myv) 
+    })
   }
-  **/
+
   //console.log(Object.entries(value))
-  const haha = 'make-fetch-happen:request-cache:https://registry.npmjs.org/memorystream/-/memorystream-0.3.1.tgz'
-  cacache.get('/home/node/.npm/_cacache', haha).then(console.log)
+  //const haha = 'make-fetch-happen:request-cache:https://registry.npmjs.org/memorystream/-/memorystream-0.3.1.tgz'
+  //cacache.get('/home/node/.npm/_cacache', haha).then(console.log)
   //Object.keys(value).forEach((item, index) => {
     //cacache.put('/home/node/mystuff', item)
   //})
 })
 
+console.log('lets see whats inside.....')
+cacache.ls('/home/mystuffycache').then(console.log)
