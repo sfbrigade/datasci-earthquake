7171a0ad2e17ff7f418032c48aad34b03e1b1224
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2026-03-11 22:31:56 -0700

    improve typing of map options

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -1,23 +1,23 @@
 "use client";
 
 import React, { useRef, useEffect } from "react";
-import mapboxgl, { LngLat } from "mapbox-gl";
+import mapboxgl, { LngLat, LngLatLike, MapOptions } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { FeatureCollection, Geometry } from "geojson";
 import { toaster } from "@/components/ui/toaster";
 import { LayerToggleObjProps } from "./address-mapper";
 
-const defaultCoords = [-122.408020683, 37.801698301]; // TODO: dedupe with address-mapper default coords; consider centralizing in a constants file if we need to use in more places
-const mapOptions = {
+const defaultCoords: LngLatLike = [-122.408020683, 37.801698301]; // TODO: dedupe with address-mapper default coords; consider centralizing in a constants file if we need to use in more places
+const mapOptions: Omit<MapOptions, "container"> = {
   style: "mapbox://styles/mapbox/standard",
-  center: defaultCoords as [number, number],
+  center: defaultCoords,
   zoom: 12.1, // Start with more zoomed-out view but not too far
   minZoom: 11, // Allow users to zoom out more
   maxZoom: 15, // Increase max zoom to allow closer inspection
   maxBounds: [
     [-122.6, 37.65], // Southwest coordinates
     [-122.25, 37.85], // Northeast coordinates
-  ] as [[number, number], [number, number]],
+  ],
   dragRotate: false, // turn off rotation on drag
   touchPitch: false, // turn off pitch change w/touch
   touchZoomRotate: true, // turn on zoom/rotate w/touch
