2546ac27ccdea5f9cd1f04a507e6aa6dbc769ef3
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2026-03-17 15:57:22 -0700

    add a link to webapp from Storybook

--- .storybook/safehome-theme.ts
+++ .storybook/safehome-theme.ts
@@ -1,9 +1,18 @@
 import { create } from "storybook/theming";
 
+let appLink = "";
+
+if (process.env.STORYBOOK_NODE_ENV === "development") {
+  // calculate the link dynamically
+  const currentHost = window.location.hostname;
+  appLink = `http://${currentHost}:3000`;
+} else {
+  appLink = process.env.STORYBOOK_APP_URL || "https://safehome.report";
+}
+
 export default create({
   base: "dark",
-  brandTitle: "SafeHome Storybook",
-  brandUrl: "https://safehome.report",
+  brandTitle: `Navigate to ${appLink}, the SafeHome webapp for this environment`,
+  brandUrl: appLink,
   brandImage: "../public/images/SFSafeHome-fulllogo.svg",
-  brandTarget: "_self",
 });
