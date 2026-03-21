56e80a7a92882e0d69c7fbf6ba35fc1f77ef4e3b
zztop <254842763+bees-dont-have-knees@users.noreply.github.com>
2026-03-21 10:43:00 -0400

    h

--- backend/database/rrr.sql
+++ backend/database/rrr.sql
@@ -1,6 +1,9 @@
-select count(*) from landslide_zones;
-select count(*) from liquefaction_zones;
-select count(*) from soft_story_properties;
-select count(*) from tsunami_zones;
+select count(*) as land from landslide_zones;
+select count(*) as liq from liquefaction_zones;
+select count(*) as soft from soft_story_properties;
+select count(*) as tsu from tsunami_zones;
 
+select table_name, column_name, data_type from information_schema.columns where table_name = 'landslide_zones';
 select table_name, column_name, data_type from information_schema.columns where table_name = 'liquefaction_zones';
+select table_name, column_name, data_type from information_schema.columns where table_name = 'soft_story_properties';
+select table_name, column_name, data_type from information_schema.columns where table_name = 'tsunami_zones';
