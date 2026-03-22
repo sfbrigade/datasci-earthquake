44db454c1b9376272f410b4fcf6bf2a59e23d7b8
zztop <254842763+bees-dont-have-knees@users.noreply.github.com>
2026-03-22 14:07:26 -0400

    more

--- backend/populate_db.sql
+++ backend/populate_db.sql
@@ -1,1 +1,4 @@
-select * from information_schema.tables;
+select count(*) as land from landslide_zones;
+select count(*) as liq from liquefaction_zones;
+select count(*) as soft from soft_story_properties;
+select count(*) as tsu from tsunami_zones;
