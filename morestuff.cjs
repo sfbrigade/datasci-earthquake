781d10f8a31812bdd7678f8d4f1abca6cb53272c
zztop <254842763+bees-dont-have-knees@users.noreply.github.com>
2026-03-28 14:58:12 -0400

    hu

--- morestuff.cjs
+++
@@ -1,5 +0,0 @@
-const cacache = require('cacache')
-
-console.log('see what happens....')
-cacache.ls('/app/drinksonyou').then(console.log)
-
--- stuff.js
+++
@@ -1,3 +0,0 @@
-const cacache = require('cacache')
-
-console.log('haha oh....hi')
