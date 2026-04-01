ed7aef54a8e02f373956590c7c0ecfe1753bed1b
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2026-04-01 16:33:20 -0700

    replace responsive changes with a more focused set of changes and improve map centering     - simplify useEffect that calls updateHazardData     - derive initial display data instead of setting state  synchronously; also add guard so that the last fetch wins  and data is not set when component is unmounted     - tweak hazard cards to change at md breakpoint instead of sm breakpoint     - remove extraneous coordinates state from address mapper and replace with derived value     - get rid of duplicate defaultCoords and rely on passed in coords as default center (fall back to map style's center, if defined, otherwise)     - slow down map panning to decrease marker jank     - tidy up search bar a bit     - fix legend dropdown from showing at medium breakpoint and above     - also use Chakra breakpoint handling for responsive breakpoints     - also toggle map marker on and off depending on if there's an address     - also fix some lint errors and warnings along the way     - also add extra note to custom next link; also add extra logging to fetch-data.ts and enable prettier for its folder

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import React, { useRef, useEffect } from "react";
-import mapboxgl, { LngLat, LngLatLike, MapOptions } from "mapbox-gl";
+import mapboxgl, { LngLat, MapOptions } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { FeatureCollection, Geometry } from "geojson";
 import { toaster } from "@/components/ui/toaster";
 import { LayerToggleObjProps } from "./address-mapper";
 
-const defaultCoords: LngLatLike = [-122.437, 37.768]; // TODO: dedupe with address-mapper default coords; consider centralizing in a constants file if we need to use in more places
 const mapOptions: Omit<MapOptions, "container"> = {
   style: "mapbox://styles/mapbox/standard",
-  center: defaultCoords,
   zoom: 12.1, // Start with more zoomed-out view but not too far
   minZoom: 11, // Allow users to zoom out more
   maxZoom: 15, // Increase max zoom to allow closer inspection
@@ -30,42 +28,46 @@ const mapOptions: Omit<MapOptions, "container"> = {
   },
 };
 interface MapProps {
-  coordinates: number[];
+  initialCenter?: number[];
+  address: string | null;
   softStoryData: FeatureCollection<Geometry>;
   tsunamiData: FeatureCollection<Geometry>;
   liquefactionData: FeatureCollection<Geometry>;
   layerToggleObj: LayerToggleObjProps;
 }
 
+const addMarker = (center: LngLat, map: mapboxgl.Map) => {
+  const el = document.createElement("div");
+
+  return new mapboxgl.Marker({
+    anchor: "bottom",
+    element: el,
+    className: "marker",
+  })
+    .setLngLat(center)
+    .addTo(map);
+};
+
 const Map: React.FC<MapProps> = ({
-  coordinates = defaultCoords,
+  initialCenter,
+  address,
   softStoryData,
   tsunamiData,
   liquefactionData,
   layerToggleObj,
 }: MapProps) => {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  const mapRef = useRef<mapboxgl.Map>(undefined);
-  const markerRef = useRef<mapboxgl.Marker>(undefined);
+  const mapRef = useRef<mapboxgl.Map>(null);
+  const markerRef = useRef<mapboxgl.Marker>(null);
   const toastIdInvalidToken = "invalid-token";
   const toastIdNoToken = "no-token";
+  const initialLon = initialCenter ? initialCenter[0] : null;
+  const initialLat = initialCenter ? initialCenter[1] : null;
 
-  const handleToggleLayers = () => {
-    if (!mapRef.current) return;
-    const map = mapRef.current;
-
-    const layerId = layerToggleObj.layerId;
-
-    if (layerId && map.getLayer(layerId)) {
-      const newVisibility = layerToggleObj.toggleState ? "visible" : "none";
-      // sets new visibility property value for layer, creating the "toggling" effect
-      map.setLayoutProperty(layerId, "visibility", newVisibility);
-    }
-  };
-
+  // TODO: how do we simplify this `useEffect()` without ill side effects like map repainting by e.g. breaking it up into multiples or moving anything outside of it? for example, can anything be derived on render instead? or can anything run in a useEffect that runs only in initial render with an empty array w/out complicating subsequent update logic?
   useEffect(() => {
+    // TODO: can this if and the token assignment be moved into render? (without the early return); seems like it's just derived logic
     const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
-
     if (!mapContainerRef.current || !mapboxToken) {
       if (!toaster.isVisible(toastIdNoToken)) {
         toaster.create({
@@ -79,14 +81,17 @@ const Map: React.FC<MapProps> = ({
       console.error("Mapbox access token or container is not set!");
       return;
     }
-
     mapboxgl.accessToken = mapboxToken;
 
+    const center =
+      initialLon && initialLat ? new LngLat(initialLon, initialLat) : null;
+
     if (!mapRef.current) {
-      // initial pass: render map
       // TODO: look into adding "testMode: true" via eg `testMode: process.env.NODE_ENV === 'test'` for tests and/orCI environment; this mode uses no access token nor does it have WebGL visual output, so styles will have to be loaded from local sources)
+      // initial pass: create map
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current!,
+        ...(center ? { center } : {}), // use provided initial center if it exists, otherwise rely on default center in map style, if it exists
         ...mapOptions,
       });
 
@@ -97,22 +102,13 @@ const Map: React.FC<MapProps> = ({
       const nav = new mapboxgl.NavigationControl({ showCompass: false });
       map.addControl(nav, "bottom-right");
 
+      if (center && address) {
+        console.log("set up markerRef.current for first time");
+        markerRef.current = addMarker(center, map);
+      }
+
       // wait for map to load before drawing marker
       map.on("load", () => {
-        // Draw address marker
-        const el = document.createElement("div");
-
-        const addressLngLat = new LngLat(coordinates[0], coordinates[1]);
-        const addressMarker = new mapboxgl.Marker({
-          anchor: "bottom",
-          element: el,
-          className: "marker",
-        })
-          .setLngLat(addressLngLat)
-          .addTo(map);
-
-        markerRef.current = addressMarker;
-
         // Add sources
         map.addSource("seismic", { type: "geojson", data: liquefactionData });
 
@@ -174,14 +170,57 @@ const Map: React.FC<MapProps> = ({
     } else {
       // subsequent passes: update map
       const map = mapRef.current;
-      const addressLngLat = new LngLat(coordinates[0], coordinates[1]);
-      map.panTo(addressLngLat);
-      markerRef.current?.setLngLat(addressLngLat);
+
+      // only show map marker if there are proper coordinates and address
+      if (center) {
+        // coordinates exist
+        if (address) {
+          // address exists
+          if (!markerRef.current) {
+            // map marker does not yet exist, so create it
+            console.log("set up markerRef.current for subsequent time");
+            markerRef.current = addMarker(center, map);
+          }
+          console.log("update markerRef.current");
+          markerRef.current.setLngLat(center);
+        } else {
+          // no address, so remove map marker if it exists
+          if (markerRef.current) {
+            console.log(
+              "removing markerRef.current marker from map and setting to null"
+            );
+            markerRef.current?.remove();
+            markerRef.current = null;
+          }
+        }
+        map.panTo(center, { duration: 750 }); // pan to new center
+      }
+
       return;
     }
-  }, [coordinates, liquefactionData, softStoryData, tsunamiData]);
+  }, [
+    initialLon,
+    initialLat,
+    address,
+    liquefactionData,
+    softStoryData,
+    tsunamiData,
+  ]);
 
   useEffect(() => {
+    const handleToggleLayers = () => {
+      if (!mapRef.current) return;
+      const map = mapRef.current;
+
+      const layerId = layerToggleObj.layerId;
+
+      if (layerId && map.getLayer(layerId)) {
+        const newVisibility = layerToggleObj.toggleState ? "visible" : "none";
+        // sets new visibility property value for layer, creating the "toggling" effect
+        map.setLayoutProperty(layerId, "visibility", newVisibility);
+      }
+    };
+
     if (layerToggleObj.layerId != "") handleToggleLayers();
   }, [layerToggleObj]); // re-runs every time state changes
 
