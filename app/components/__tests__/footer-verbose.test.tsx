6b8a4a487cd9644d445804c908f4c8b70b161bb0
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2026-04-22 04:16:48 -0700

    fix unit tests and update copyright year - also remove extraneous commented-out code in `layout-repsonsive.tsx` - also format `ImageTextCard.stories.ts`

--- app/components/__tests__/footer-verbose.test.tsx
+++ app/components/__tests__/footer-verbose.test.tsx
@@ -1,19 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "../ui/provider";
 import "@testing-library/jest-dom";
-import Footer from "../footer-verbose";
+import FooterVerbose from "../footer-verbose";
 import "../__mocks__/match-media";
 
 describe("Footer Component", () => {
   const renderFooter = () =>
     render(
       <Provider>
-        <Footer />
+        <FooterVerbose />
       </Provider>
     );
 
   it("renders the logo", () => {
     renderFooter();
-    expect(screen.getByText("© 2025 SF Civic Tech")).toBeInTheDocument();
+    expect(screen.getByText("© 2026 SF Civic Tech")).toBeInTheDocument();
   });
 });
