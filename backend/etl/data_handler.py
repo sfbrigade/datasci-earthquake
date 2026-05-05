6361c692c056d9d30b668f185702c8b083672c0d
zztop <254842763+bees-dont-have-knees@users.noreply.github.com>
2026-05-05 13:20:58 -0400

    init

--- backend/etl/data_handler.py
+++ backend/etl/data_handler.py
@@ -60,7 +60,8 @@ class DataHandler(ABC):
         self.table = table
         self.db_getter = get_db
         self.page_size = page_size
-        self.logger = logging.getLogger(f"{self.__class__.__name__}")
+        #self.logger = logging.getLogger(f"{self.__class__.__name__}")
+        self.logger = logging.getLogger(__name__)
         self.session = session or SessionManager.create_session()
         self.request_handler = RequestHandler(self.session)
         try:
