1e8a0f76c1c130dcee30d44327d731d454115b35
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2026-05-05 19:05:42 -0700

    format storybook main config to fix prettier warnings

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
-import { mergeConfig } from 'vite';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import { mergeConfig } from "vite";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 
 // Recreate __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -33,21 +33,21 @@ const config: StorybookConfig = {
     options: {},
   },
   refs: {
-    'design-system': {
-      title: 'Storybook Design System',
-      url: 'https://master--5ccbc373887ca40020446347.chromatic.com/',
+    "design-system": {
+      title: "Storybook Design System",
+      url: "https://master--5ccbc373887ca40020446347.chromatic.com/",
       expanded: false, // Optional, true by default
     },
   },
-  staticDirs: ['./public'], // Relative to the .storybook folder
+  staticDirs: ["./public"], // Relative to the .storybook folder
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
       //production builds use `/storybook` path pointing to the static Storybook build whereas development will simply use the default `/` path on its own localhost port
-      base: configType === 'PRODUCTION' ? '/storybook/' : '/',
+      base: configType === "PRODUCTION" ? "/storybook/" : "/",
       resolve: {
         // add an explicit manual fallback for the @ alias that's equivalent to what's in tsconfig.json; this is needed to get the imports working in Storybook for the UI provider and theme (and will likely be needed for other imports as well)
         alias: {
-          '@': path.resolve(__dirname, '../src'),
+          "@": path.resolve(__dirname, "../src"),
         },
       },
     });
