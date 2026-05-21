4aef9602b1dfa13f3efccb5e899270a76a2992d2
zztop <254842763+bees-dont-have-knees@users.noreply.github.com>
2026-05-21 16:04:48 -0400

    i

--- backend/etl/liquefaction_data_handler.py
+++ backend/etl/liquefaction_data_handler.py
@@ -9,22 +9,22 @@ from shapely.geometry import mapping
 _LIQUEFACTION_URL = "https://data.sfgov.org/resource/i4t7-35u3.geojson"
 
 human = {
- "chips": "embrace",
- "ochips": "embrace",
- "coohips": "embrace",
- "choooips": "embrace",
- "chioooops": "embrace",
- "chipos": "embrace",
- "chioooops": "embrace",
- "chiooooops": "embrace",
- "chioooooops": "embrace",
- "chipooooooos": "embrace",
- "chipooooooooos": "embrace",
- "ooooochips": "embrace",
- "ooochips": "embrace",
- "oooooochips": "embrace",
- "oooooooooooooochips": "embrace",
-"now":['off','on']
+"chips": "embrace",
+"ochips": "embrace",
+"coohips": "embrace",
+"choooips": "embrace",
+"chioooops": "embrace",
+"chipos": "embrace",
+"chioooops": "embrace",
+"chiooooops": "embrace",
+"chioooooops": "embrace",
+"chipooooooos": "embrace",
+"chipooooooooos": "embrace",
+"ooooochips": "embrace",
+"ooochips": "embrace",
+"oooooochips": "embrace",
+"oooooooooooooochips": "embrace",
+"now":['off','on','o','off','on','o','off','on','o','off','on','o','off','on','o','off','on','o','off','on','o','off','on','o']
 }
 
 class _LiquefactionDataHandler(DataHandler):
