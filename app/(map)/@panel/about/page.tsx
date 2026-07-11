a33e4433eb24772ec68f87ad873c2ee9e6aa4374
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2026-07-10 19:07:04 -0700

    move right panel into parallel routes to allow for loading without full page loads
    Co-authored-by: nutanp <nutan.pandeya@gmail.com>

---
+++ app/(map)/@panel/about/page.tsx
@@ -0,0 +1,10 @@
+import { PanelWrapper } from "../panel-wrapper";
+import AboutUs from "@/components/about-us";
+
+export default function PreparePage() {
+  return (
+    <PanelWrapper>
+      <AboutUs />
+    </PanelWrapper>
+  );
+}
---
+++ app/(map)/@panel/prepare/page.tsx
@@ -0,0 +1,10 @@
+import { PanelWrapper } from "../panel-wrapper";
+import Prepare from "@/components/prepare";
+
+export default function PreparePage() {
+  return (
+    <PanelWrapper>
+      <Prepare />
+    </PanelWrapper>
+  );
+}
