daeb8420b5d2feb43edd23846265ac7b106f77ed
nutanp <nutan.pandeya@gmail.com>
2026-07-13 18:51:46 -0700

    fix responsiveness of prepare panel
    Co-authored-by :  Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>

--- app/(map)/@panel/panel-wrapper.tsx
+++ app/(map)/@panel/panel-wrapper.tsx
@@ -1,27 +1,44 @@
-import { Box } from "@chakra-ui/react";
+"use client";
+import { Box, CloseButton, Float } from "@chakra-ui/react";
+import { useSearchParams } from "next/navigation";
+import NextLink from "@/components/custom-next-link";
 
 interface PanelWrapperProps {
   children: React.ReactNode;
 }
 
-export const PanelWrapper = ({ children }: PanelWrapperProps) => (
-  <Box
-    id="panel-container"
-    as="section"
-    aria-label="Right panel"
-    position="absolute"
-    top="0"
-    right="0"
-    bottom="0"
-    zIndex="overlay"
-    backgroundColor="white"
-    overflowY="auto"
-    overflowX="hidden"
-    boxShadow="md"
-    flexShrink={0}
-    w={{ base: "full", md: "2/5" }}
-    h="full"
-  >
-    {children}
-  </Box>
-);
+export const PanelWrapper = ({ children }: PanelWrapperProps) => {
+  const searchParams = useSearchParams();
+
+  // TODO:merge this logic with getNavigationHref
+
+  const currentQueryString = searchParams.toString();
+  const homeHref = currentQueryString ? `/?${currentQueryString}` : "/";
+  return (
+    <Box
+      id="panel-container"
+      as="section"
+      aria-label="Right panel"
+      position="absolute"
+      top="0"
+      right="0"
+      bottom="0"
+      zIndex="overlay"
+      backgroundColor="white"
+      overflowY="auto"
+      overflowX="hidden"
+      boxShadow="md"
+      flexShrink={0}
+      w={{ base: "full", md: "2/5" }}
+      h="full"
+    >
+      <Float offset="8">
+        <NextLink href={homeHref}>
+          <CloseButton />
+        </NextLink>
+      </Float>
+
+      {children}
+    </Box>
+  );
+};
