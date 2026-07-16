5bb601f9218c524479cb15e22e97fe6765808e7e
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2026-07-16 12:24:59 -0700

    tweak social link whitespace

--- app/components/social-links.tsx
+++ app/components/social-links.tsx
@@ -13,7 +13,7 @@ export function SocialLinks() {
       >
         Contact Us
       </Text>
-      <HStack>
+      <HStack gap="3.5">
         <IconButton asChild aria-label="LinkedIn" variant="ghost">
           <a
             href="https://www.linkedin.com/company/safehome-civictech"
