dc9fe3c276f156ba41fc949a378716edf0497c42
Nick Visutsithiwong <2823112+nickvisut@users.noreply.github.com>
2026-09-14 20:11:02 -0700

    feat: add FEMA layer

--- app/api/services.ts
+++ app/api/services.ts
@@ -9,3 +9,6 @@ export const fetchTsunami = async () =>
 
 export const fetchLiquefaction = async () =>
   fetchData(CDN_ENDPOINTS.liquefaction, API_ENDPOINTS.liquefaction);
+
+export const fetchFema = async () =>
+  fetchData(CDN_ENDPOINTS.fema, API_ENDPOINTS.fema);
