60ead84184f6d058a54d9f1590fb1cdd29cda98b
nutanp <nutan.pandeya@gmail.com>
2026-09-16 22:22:16 -0700

    feat: work on hazard card

--- app/components/__tests__/address-mapper.test.tsx
+++ app/components/__tests__/address-mapper.test.tsx
@@ -64,6 +64,7 @@ const mockProps = {
   softStoryData: mockFeatureCollection,
   tsunamiData: mockFeatureCollection,
   liquefactionData: mockFeatureCollection,
+  femaRiskData: mockFeatureCollection,
 };
 
 const mockSetSearchParams = (params: Record<string, string>) => {
